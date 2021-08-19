2667f0d9debacba96cf6a59dc02c44611c7e19b5
cbrl1974 <lcabral@tailbase.com>
2021-08-19 09:39:56 -0700

    Update query for Alliance

--- Shopping Carts/Alliance Furnishings Cart queries.sql
+++ Shopping Carts/Alliance Furnishings Cart queries.sql
@@ -36,16 +36,24 @@ for json path
 
 --Script to poulate the shipping options shared document for alliance furnishings
 DECLARE @MerchantIdZones AS INT = 1097;
---select * from merchantzones where merchant_id =@MerchantIdZones
+select * from merchantzones where merchant_id =@MerchantIdZones
 select mz.name_1,mzc.stateCode, mzc.postalCode from merchantzonecodes mzc
 join merchantzones mz on mz.id = mzc.merchantzoneId
 where mzc.merchantzoneid in (select id from merchantzones where merchant_id = @MerchantIdZones)
-and mzc.postalCode = '70502'
 
 
 
---Finding duplicated zipcodes on delivery zones
-WITH CTE AS 
+--*****************Finding duplicated zipcodes on delivery zones************************
+
+WITH CTE_zones (id, merchant_id, name_1) as (
+    select id, merchant_id, name_1 from merchantzones
+),
+ CTE_merchantZoneCodes AS 
+(
+     select merchantzoneid, stateCode, postalCode
+     from merchantZoneCodes
+),
+CTE_zone_duplicates AS 
 (
     SELECT  postalcode
     from merchantzonecodes 
@@ -54,16 +62,21 @@ WITH CTE AS
     HAVING COUNT(postalcode) > 1
 )
 
-select mz.name_1,mzc.stateCode, mzc.postalCode from merchantzonecodes mzc
-join merchantzones mz on mz.id = mzc.merchantzoneId
-join CTE ct on ct.postalCode = mzc.postalCode
-where mzc.merchantzoneid in (select id from merchantzones where merchant_id = 1097) 
+select z.name_1, zc.stateCode, zc.postalCode from CTE_zones z
+join CTE_merchantZoneCodes zc on zc.merchantzoneid = z.id
+join CTE_zone_duplicates d on d.postalCode = zc.postalCode
+where z.merchant_id = 1097
 
+
+--*********************Delete Duplicates**********************
 WITH CTE AS
 (
-SELECT *,ROW_NUMBER() OVER (PARTITION BY postalcode ORDER BY postalcode) AS RN
-from merchantzonecodes 
-where merchantzoneid = 1051
+SELECT mz.name_1,mzc.*,ROW_NUMBER() OVER (PARTITION BY postalcode ORDER BY postalcode) AS RN
+from merchantzonecodes mzc
+inner join merchantzones mz on mz.id = mzc.merchantZoneId
+where merchantzoneid in (select id from merchantZones where merchant_id = 1097 and name_1 <> 'No Zone/Zone 0')
 )
 
-DELETE FROM CTE WHERE RN<>1
+select * FROM CTE WHERE RN <> 1
+--DELETE FROM CTE WHERE RN<>1
+
