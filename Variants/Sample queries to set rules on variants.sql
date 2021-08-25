7eab55a03205ff2a8115ef5b1933a484897d8a40
cbrl1974 <lcabral@tailbase.com>
2021-08-25 09:18:15 -0700

    query to update system settings

--- Variants/Sample queries to set rules on variants.sql
+++ Variants/Sample queries to set rules on variants.sql
@@ -1,16 +1,20 @@
-exec VariantsSuperGroup 110863
+exec VariantsSuperGroup 680480
 
 select [dbo].[FastLevenshtein] (
-    'SoundLink Bluetooth Mobile Speaker II (S)',
-    'SoundLink wireless mobile speaker (B)',
-    --'SoundLink Mini',
+    'SRS-XB40 (Bl)',
+    'SRSBTM8 (B)',
     NULL
 )
 
-select p.id_product, p.manufid, p.manufmodel, p.model, sp.* from products p
+select p.Id_product, p.manufid, p.manufmodel, p.model, sp.SpecValue_1 from products p
 inner join SpecsProd sp on sp.productID =  p.Id_product
-where p.manufid = 183
+left join merchantProds mp on mp.ProductID = p.Id_product
+where p.manufid = 1110
 and p.catid = 633
-and p.active = 1 and p.discontinued = 0 and p.specs = 1 and sp.SpecValue_1 is not null
+and p.active = 1 and p.discontinued = 0 and p.specs = 1 
+and  sp.SpecValue_1 is not null
+and sp.specsID_1 = 27071
 
+select * from companies where cie like '%sonos%'
+select * from companies where id_cie = 321
 
