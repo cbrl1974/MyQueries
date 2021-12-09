c16f0dada0187fcb0187d9779bf75f396e6b3064
cbrl1974 <lcabral@tailbase.com>
2021-12-09 06:26:16 -0800

    update

--- Scrapers/get_data_from_scrappers.sql
+++ Scrapers/get_data_from_scrappers.sql
@@ -7,8 +7,12 @@ select * from retailers where id_retailer in (2753,2755,2756)
 
 
 DECLARE @ProductID as int = 558502;
+DECLARE @MerchantID as int = 1900;
+select price, reducedPrice from merchantprods where merchant_id = @MerchantID and productid = @ProductID
 select top 100 * from webtracking 
 where retailerID in (2753,2755,2756)
 and productid =  @ProductID
-and TrackingDate between '2021-12-08' and '2021-12-09'
+and TrackingDate between '2021-12-09' and '2021-12-10'
+
+
 
