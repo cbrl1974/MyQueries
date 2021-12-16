0c5d59e0d83a908acdc9f55d3665edf21511c38f
cbrl1974 <lcabral@tailbase.com>
2021-12-16 07:20:14 -0800

    query update

--- Scrapers/get_data_from_scrappers.sql
+++ Scrapers/get_data_from_scrappers.sql
@@ -3,7 +3,7 @@ DECLARE @MerchantID as int = 1900;
 select competition from merchants where id = @MerchantID
 
 
-select * from retailers where id_retailer in (2753,2755,2756)
+select * from retailers where webtracking = 1 and countryid = 36
 
 
 DECLARE @ProductID as int = 558502;
@@ -12,7 +12,32 @@ select price, reducedPrice from merchantprods where merchant_id = @MerchantID an
 select top 100 * from webtracking 
 where retailerID in (2753,2755,2756)
 and productid =  @ProductID
-and TrackingDate between '2021-12-09' and '2021-12-10'
+and TrackingDate between '2021-12-15' and '2021-12-16'
+
+
+
+select a.retailerid as RETAILER_ID, b.cie AS RETAILER_NAME, date , count(*) as PRODUCT_COUNT
+from Tailbaseservices.dbo.WebTrackingProducts a
+inner join [datatail20130410].dbo.companies b
+on a.RetailerID = b.Id_cie
+where cast(a.date as date)  >= '2021-12-16'
+group by a.retailerid, b.cie,date
+order by b.cie
+
+
+select * from companies where Id_cie = 3132
+
+select top 10 * from Tailbaseservices.dbo.WebTrackingProducts order by date desc
+
+
+
+select * from Tailbaseservices.dbo.AsyncTasks where AssemblyClass like '%scraper%' order by LastIntervalExecution desc
+select * from Tailbaseservices.dbo.ScrapersSettings
+select  top 10 * from Tailbaseservices.dbo.HomeDepotCanadaBrandModels
+select top 10 * from Tailbaseservices.dbo.DebugInfoScraper
+select * from Tailbaseservices.dbo.WebTrackingCategories
+select top 10 * from Tailbaseservices.dbo.WebTrackingProducts order by date desc
+select * from Tailbaseservices.dbo.ApiClientConfigs
 
 
 
