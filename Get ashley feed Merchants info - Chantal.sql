4d6c62225568b7bec4fb96075e5449f6c7d1db30
cbrl1974 <lcabral@tailbase.com>
2022-01-07 07:02:20 -0800

    Add RTO

--- Get ashley feed Merchants info - Chantal.sql
+++ Get ashley feed Merchants info - Chantal.sql
@@ -8,6 +8,9 @@ select m.id, m.merchant, SUBSTRING(mf.remoteFTPUser, 4, 6) as AccountNumber, m.m
     (
     CASE WHEN count(p.manufid) = 5 THEN 'Ashley Only' else 'Multi MFG' end
 ) as isAshley,
+(
+    CASE WHEN mc.RTOConsole = 1 THEN 'yes' else 'no' end
+) as isRTO,
     md.city,
     md.statecode,
     md.CountryName,
@@ -19,14 +22,15 @@ from merchantfeeds mf
     left join products p on p.Id_product = mp.ProductID
     left join MerchantDistributorWarehouseLocations md on md.merchantid = mf.merchant_id
     left join merchantwebsitefeatures mwf on mwf.merchant_id = mf.merchant_id 
+    inner join merchantCms mc on mc.Merchant_id = m.id
     and mwf.featurecode = 'shoppingCartType'
     left join merchantshipping ms on ms.dropShippingProgramID = md.DistributorID
 where mf.brand = 'Ashley'
     and m.active = 1
     and mf.active = 1
     and p.manufID in (1436,3181,3182,3184,4227)
---and ms.dropShippingProgramID = 1  ---added direct express
-group by m.id, m.merchant,mf.remoteFTPUser,m.merchant_url,mwf.featurecode,ms.dropShippingProgramID,md.city, md.statecode, md.CountryName, lastmodified
+and ms.dropShippingProgramID = 1  ---added direct express
+group by m.id, m.merchant,mf.remoteFTPUser,m.merchant_url,mwf.featurecode,ms.dropShippingProgramID,md.city, md.statecode, md.CountryName,mc.RTOConsole, lastmodified
 order by m.id
 
 --Ashley Brands
@@ -38,6 +42,10 @@ inner join products p on p.Id_product = mp.ProductID
 where p.manufID in (1436,3181,3182,3184,4227)
 and mp.merchant_id = 2417
 group by mp.merchant_id
+
+select * from merchantWebsitefeatures where merchant_id = 2417
+select * from merchants where id = 2417
+select RTOConsole from merchantCms where merchant_id = 2417 
 
 
 
