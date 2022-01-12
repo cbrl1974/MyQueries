a3690bfdcec80b0749ae77a049e11e59263f4a5e
cbrl1974 <lcabral@tailbase.com>
2022-01-12 10:18:57 -0800

    Update query

--- Scrapers/get_data_from_scrappers.sql
+++ Scrapers/get_data_from_scrappers.sql
@@ -4,29 +4,30 @@ select competition from merchants where id = @MerchantID
 
 
 select * from retailers where webtracking = 1 and countryid = 36
+select * from products where model like '%RF25HMIDBSR%'
 
 
-DECLARE @ProductID as int = 558502;
-DECLARE @MerchantID as int = 1900;
-select price, reducedPrice from merchantprods where merchant_id = @MerchantID and productid = @ProductID
-select top 100 * from webtracking 
-where retailerID in (2753,2755,2756)
-and productid =  @ProductID
-and TrackingDate between '2021-12-15' and '2021-12-16'
+select retailerid, sku, modelNo, RegularPrice, AdvertisedPrice
+  from TailbaseServices.dbo.WebTrackingProducts
+where retailerid = 26
+and [date] between '2022-01-12' and '2022-01-13'
+and modelNo like '%RF28T5A01SR%'
 
 
 
+--This checks if the total of products per merchant on a specific day. To check if the scrapper did ran
 select a.retailerid as RETAILER_ID, b.cie AS RETAILER_NAME, date , count(*) as PRODUCT_COUNT
 from Tailbaseservices.dbo.WebTrackingProducts a
 inner join [datatail20130410].dbo.companies b
 on a.RetailerID = b.Id_cie
-where cast(a.date as date)  = '2021-12-17'
+where cast(a.date as date)  = '2022-01-12'
 group by a.retailerid, b.cie,date
 order by b.cie
 
-
+--This checks  the state of a scraper
 select * from Tailbaseservices.dbo.AsyncTasks where AssemblyClass like '%Visions%' and AssemblyMethodName = 'ScrapeProducts'
 
+--This runs an indicidual scrapper
 update top (1) Tailbaseservices.dbo.AsyncTasks 
 set State = 1
 where id = '18DE8062-ABCE-42AA-83A0-C03049382FCB'
