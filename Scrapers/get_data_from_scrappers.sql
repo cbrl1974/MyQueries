299154fc1fbc5462ffd6e38aeb4137a3b8abf708
cbrl1974 <lcabral@tailbase.com>
2022-01-17 07:11:45 -0800

    Update query

--- Scrapers/get_data_from_scrappers.sql
+++ Scrapers/get_data_from_scrappers.sql
@@ -10,7 +10,7 @@ select * from products where model like '%RF25HMIDBSR%'
 select retailerid, sku, modelNo, RegularPrice, AdvertisedPrice, [date]
   from TailbaseServices.dbo.WebTrackingProducts
 where retailerid = 26
-and [date] between '2022-01-12' and '2022-01-13'
+and [date] between '2022-01-12' and '2022-01-17'
 and modelNo like '%RF25HMIDBSR%'
 
 
@@ -20,9 +20,13 @@ select a.retailerid as RETAILER_ID, b.cie AS RETAILER_NAME, date , count(*) as P
 from Tailbaseservices.dbo.WebTrackingProducts a
 inner join [datatail20130410].dbo.companies b
 on a.RetailerID = b.Id_cie
-where cast(a.date as date)  = '2022-01-12'
+where cast(a.date as date)  = '2022-01-17'
 group by a.retailerid, b.cie,date
 order by b.cie
+
+--Check the state of a scraper
+select assemblyclass, [state], LastIntervalExecution, [Interval] from AsyncTasks where AssemblyClass like '%scraper%'
+ order by [state] , LastIntervalExecution
 
 --This checks  the state of a scraper
 select * from Tailbaseservices.dbo.AsyncTasks where AssemblyClass like '%Leon%' and AssemblyMethodName = 'ScrapeProducts'
@@ -33,6 +37,23 @@ select * from Tailbaseservices.dbo.AsyncTasks where AssemblyClass like '%Leon%' 
 update asynctasks
 set State = 1
 where id = '283D76B5-C42E-4B8D-A921-39DEDC447C45'
+
+--LeonsScraper
+update asynctasks
+set state = 1
+where id = '38C5029F-EFB6-4E8E-B25D-EBDAD106470E'
+
+
+--HomeDepotCanadaScraper
+update asynctasks
+set State = 1
+where id = '7C2CB5E5-171A-4354-A1DE-F5E9E198B7B1'
+
+
+--LowesCanadaScraper
+update asynctasks
+set state = 1
+where id = 'CFA1D4BB-9F88-4A93-A0EE-AA40A1E9B607'
 
 
 --RonaScraper
@@ -46,16 +67,11 @@ update asynctasks
 set state= 1
 where id = 'EDF195ED-B671-4AF6-8048-824306FD1E08'
 
---BestBuyUsScraper
-update asynctasks
-set state= 1
-where id = '556BCCBA-7C26-4732-9DA0-82D9D80CC1C1'
 
-
---HomeDepotUsScraper
+--Visions
 update asynctasks
-set State = 1
-where id = 'A84C8FD6-B708-446B-BA86-95086E76FD0B'
+set state = 1
+where id = '18DE8062-ABCE-42AA-83A0-C03049382FCB'
 
 
 --MesElectrosScraper
@@ -70,34 +86,11 @@ set State = 1
 where id = 'BBF0B852-5967-4278-B302-A9D3747F7C87'
 
 
---LowesCanadaScraper
-update asynctasks
-set state = 1
-where id = 'CFA1D4BB-9F88-4A93-A0EE-AA40A1E9B607'
-
-
 --RenoDepotScraper
 update asynctasks
 set state = 1
 where id = '9CA939FF-B5E8-42CE-A9F0-AD04BEFDE670'
 
-
---LeonsScraper
-update asynctasks
-set state = 1
-where id = '38C5029F-EFB6-4E8E-B25D-EBDAD106470E'
-
-
---HomeDepotCanadaScraper
-update asynctasks
-set State = 1
-where id = '7C2CB5E5-171A-4354-A1DE-F5E9E198B7B1'
-
-
---LowesUsScraper
-update asynctasks
-set state = 1
-where id = '3FF2B78B-5CE8-465F-942B-F62AF9AB9A6D'
 
 
 --TheBrickScraper
@@ -118,46 +111,64 @@ set state = 1
 where id = 'B9D227DC-8FFE-48BB-A983-806C4EC1D30F'
 
 
---Staples
-update asynctasks
-set state = 1
-where id = 'F98DE0B9-7AFA-4FDC-AF18-1A00D794BD0B'
-
-
---TheBay
-update asynctasks
-set state = 1
-where id = '6291fd82-5229-459b-bc78-f8dd9d7ebe8e'
-
-
---Visions
-update asynctasks
-set state = 1
-where id = '18DE8062-ABCE-42AA-83A0-C03049382FCB'
-
-
 --CorbeilElectroScraper
 update asynctasks
 set state = 1
 where id = 'b06311af-3c7e-406f-aeac-6037c8fea39a'
 
 
+
 --Tanguay
 update asynctasks
-set lset state = 1
+set  state = 1
 where id = '4D8C9AEE-E07F-4090-936C-0E49EF8B64EB'
 
 
---The Source
+--The Source (no need to run)
 update asynctasks
 set lastintervalexecution = set state = 1
 where id = '425D9ECB-3D1C-47DE-9B07-20C105908682'
 
 
---Amazon
+--Amazon (no need to run)
 update asynctasks
 set lastintervalexecution = set state = 1
 where id = 'E67BEB31-D352-4981-B164-C3A501748555'
+
+
+--Staples (no need to run)
+update asynctasks
+set state = 1
+where id = 'F98DE0B9-7AFA-4FDC-AF18-1A00D794BD0B'
+
+
+--TheBay (no need to run)
+update asynctasks
+set state = 1
+where id = '6291fd82-5229-459b-bc78-f8dd9d7ebe8e'
+
+
+
+--BestBuyUsScraper
+update asynctasks
+set state= 1
+where id = '556BCCBA-7C26-4732-9DA0-82D9D80CC1C1'
+
+
+--HomeDepotUsScraper
+update asynctasks
+set State = 1
+where id = 'A84C8FD6-B708-446B-BA86-95086E76FD0B'
+
+
+--LowesUsScraper
+update asynctasks
+set state = 1
+where id = '3FF2B78B-5CE8-465F-942B-F62AF9AB9A6D'
+
+
+
+
 
 
 --States Defintiion
@@ -167,13 +178,20 @@ where id = 'E67BEB31-D352-4981-B164-C3A501748555'
     6 = finished executing successfully
 
 
-select * from companies where Id_cie = 3132
+
 
 select top 10 * from Tailbaseservices.dbo.WebTrackingProducts order by date desc
 
 
 
-select * from Tailbaseservices.dbo.AsyncTasks where AssemblyClass like '%scraper%' order by LastIntervalExecution desc
+
+
+
+
+
+
+
+
 select * from Tailbaseservices.dbo.ScrapersSettings
 select  top 10 * from Tailbaseservices.dbo.HomeDepotCanadaBrandModels
 select top 10 * from Tailbaseservices.dbo.DebugInfoScraper
