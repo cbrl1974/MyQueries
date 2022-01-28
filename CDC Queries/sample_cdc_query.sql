c589326b140de2e0bdee650ee8b0cab7ed99a004
cbrl1974 <lcabral@tailbase.com>
2022-01-28 09:47:12 -0800

    Update and remove

--- CDC Queries/sample_cdc_query.sql
+++
@@ -1,3 +0,0 @@
-select top 100 * from [cdc].[dbo_MerchantShipping_CT] lsnData 
-INNER JOIN [cdc].[lsn_time_mapping] lsnTime On lsnData.__$start_lsn = lsnTime.start_lsn
-where merchant_id = 3227
--- CDC Queries/some_cdc_query.sql
+++ CDC Queries/some_cdc_query.sql
@@ -15,3 +15,9 @@ order by lsnTime.tran_end_time desc
 
 
 
+select top 100 * from [cdc].[dbo_MerchantShipping_CT] lsnData 
+INNER JOIN [cdc].[lsn_time_mapping] lsnTime On lsnData.__$start_lsn = lsnTime.start_lsn
+where merchant_id = 3227
+
+
+
