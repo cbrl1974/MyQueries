ec01517bafa1ee7fc4eab2525c714d2aa285a4e5
cbrl1974 <lcabral@tailbase.com>
2022-02-08 07:59:30 -0800

    New Shopify query

---
+++ shopify/Shopify_general_queries.sql
@@ -0,0 +1,13 @@
+select * from shopify.shopifyMerchants where merchantid = 2887
+select * from MerchantExports where merchantid = 2887
+select * from shopify.ShopifyConvertReports where merchantid = 2887
+
+select * from shopify.ShopifySyncReports where merchantid = 2887
+select * from shopify.shopifyProducts where merchantid = 2887
+select top 10 * from logs order by LogTime desc
+
+select * from SyncStatus
+
+select  * from DevTailbaseCore.dbo.TaApplianceEpassParentProducts
+select  * from DevTailbaseCore.dbo.TaApplianceEpassChildProducts
+select * from shopify.shopifyProducts where merchantid = 2887
