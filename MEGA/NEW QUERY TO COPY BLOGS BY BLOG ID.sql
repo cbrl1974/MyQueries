59911719cd184dc2b42653b4eb4c9771b74dab3a
cbrl1974 <lcabral@tailbase.com>
2022-03-04 10:26:47 -0800

    Update

--- MEGA/NEW QUERY TO COPY BLOGS BY BLOG ID.sql
+++ MEGA/NEW QUERY TO COPY BLOGS BY BLOG ID.sql
@@ -1,28 +1,32 @@
 use datatail20130410
-DECLARE @MerchantId AS INT = 2791;
+DECLARE @MerchantId AS INT = 1832;
 
 
-select * from MerchantBlogs where merchantid = 2791
+select * from MerchantBlogs where merchantid = @MerchantId
 
-select * from  MerchantBlogArticles where id = 6165 --(get the articleid)
+select * from  MerchantBlogArticles where merchantid = 3388 --(get the articleid)
 
-select * from MerchantBlogTags where merchantblogid = 253 -- (has to be merchantblogid)
+select * from MerchantBlogTags where merchantblogid = 261 -- (has to be merchantblogid)
 
-select * from MerchantBlogTexts where merchantblogid = 253 -- (has to be merchantblogid)
+select * from MerchantBlogTexts where merchantblogid = 261 -- (has to be merchantblogid)
 
-select * from MerchantBlogArticlesByCategories where MerchantBlogArticleID = 6165  --(has to be MerchantBlogArticleID)
+select * from MerchantBlogArticlesByCategories 
+where MerchantBlogArticleID in (select id from  MerchantBlogArticles where merchantid = 3388)  --(has to be MerchantBlogArticleID)
 
-select * from MerchantBlogArticlesByCategories where MerchantBlogArticleID = 6165  --(has to be MerchantBlogArticleID)
+select * from MerchantBlogArticlesByCategories
+where MerchantBlogArticleID in (select id from  MerchantBlogArticles where merchantid = 3388) --(has to be MerchantBlogArticleID)
 
-select * from merchantBlogArticlesByTags where MerchantBlogArticleID = 6165  --(has to be MerchantBlogArticleID)
+select * from merchantBlogArticlesByTags
+where MerchantBlogArticleID in (select id from  MerchantBlogArticles where merchantid = 3388) --(has to be MerchantBlogArticleID)
 
-select * from MerchantBlogRelatedArticles  where MerchantBlogArticleID = 6165  --(has to be MerchantBlogArticleID)
+select * from MerchantBlogRelatedArticles  
+where MerchantBlogArticleID in (select id from  MerchantBlogArticles where merchantid = 3388) --(has to be MerchantBlogArticleID)
 
-select * from MerchantBlogCategories where merchantblogid = 253 -- (has to be merchantblogid)
+select * from MerchantBlogCategories where merchantblogid = 261 -- (has to be merchantblogid)
 
-select * from MerchantBlogAuthors where merchantblogid = 253 -- (has to be merchantblogid)
+select * from MerchantBlogAuthors where merchantblogid = 261 -- (has to be merchantblogid)
 
-select * from MerchantBlogSettings where merchantblogid = 253 -- (has to be merchantblogid) 
+select * from MerchantBlogSettings where merchantblogid = 226 -- (has to be merchantblogid) 
 
 
 
