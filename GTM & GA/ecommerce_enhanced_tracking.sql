5fa59d98796dafca2fb57896bb61e8797e93be38
cbrl1974 <lcabral@tailbase.com>
2022-03-04 10:16:54 -0800

    Update

--- GTM & GA/ecommerce_enhanced_tracking.sql
+++ GTM & GA/ecommerce_enhanced_tracking.sql
@@ -1,7 +1,15 @@
-DECLARE @MerchantID as int = 3372;
-DECLARE @GoogleAnalyticsID as varchar(20) = 'UA-159292788-1';
+DECLARE @MerchantID as int = 3331;
+DECLARE @GoogleAnalyticsID as varchar(20) = 'UA-213335548-1';
+DECLARE @HasEcommerceCode as varchar(20) = 'hasEcommerce';
+DECLARE @GoogleAnalyticsCode as varchar(20) = 'googleAnalyticsID';
+
+--Check If exists
+select * from  merchantwebsitefeatures
+where merchant_id = @MerchantID
+and featurecode in (@HasEcommerceCode, @GoogleAnalyticsCode)
+
 
 insert into merchantwebsitefeatures values 
-(@MerchantID,'hasEcommerce', NULL),
-(@MerchantID,'googleAnalyticsID',@GoogleAnalyticsID)
+(@MerchantID,@HasEcommerceCode, NULL),
+(@MerchantID,@GoogleAnalyticsCode,@GoogleAnalyticsID)
 
