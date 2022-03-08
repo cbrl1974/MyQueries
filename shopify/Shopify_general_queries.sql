f7772b676def5b514f18a5ac7bb76b3645075a2a
cbrl1974 <lcabral@tailbase.com>
2022-03-08 08:00:17 -0800

    update

--- shopify/Shopify_general_queries.sql
+++ shopify/Shopify_general_queries.sql
@@ -1,13 +1,18 @@
 select * from shopify.shopifyMerchants where merchantid = 2798
-select * from shopify.shopifyProducts where merchantid = 2798 and vendor = 'blomberg' and tailbaseID = 
+select * from shopify.shopifyProducts where merchantid = 2798
+
 select * from MerchantExports where merchantid = 2798
 select * from shopify.ShopifyConvertReports where merchantid = 2798 order by EndTime desc
-
 select * from shopify.ShopifySyncReports where merchantid = 2798
+
 select * from shopify.shopifyProducts where merchantid = 2798
-select top 10 * from logs order by LogTime desc
+select top 100 * from tailbasify.dbo.logs
+where merchantid = 2798
+order by id desc
 
 select * from SyncStatus
+
+
 
 select  * from DevTailbaseCore.dbo.TaApplianceEpassParentProducts
 select  * from DevTailbaseCore.dbo.TaApplianceEpassChildProducts
