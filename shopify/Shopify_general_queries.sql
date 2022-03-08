54be02555d1e7c6c9bfe90a7583e41a98a6ee7de
cbrl1974 <lcabral@tailbase.com>
2022-03-08 09:28:17 -0800

    update query

--- shopify/Shopify_general_queries.sql
+++ shopify/Shopify_general_queries.sql
@@ -1,19 +1,31 @@
+-- Merchants
 select * from shopify.shopifyMerchants where merchantid = 2798
+select  * from shopify.ShopifyMerchantConfigurations where ShopifyMerchantId = 7
+select * from shopify.ShopifyConfigurations order by id
+
+--Configs
+select  sc.id, c.Configuration, sc.ConfigurationValue from shopify.ShopifyMerchantConfigurations sc
+inner join shopify.ShopifyConfigurations c on c.Id = sc.ShopifyConfigurationId
+where ShopifyMerchantId = 7
+
 select * from shopify.shopifyProducts where merchantid = 2798
 
+--Exports & Reports
 select * from MerchantExports where merchantid = 2798
+
+update top (1) MerchantExports
+set [status] = 1
+where merchantid = 2798
+
 select * from shopify.ShopifyConvertReports where merchantid = 2798 order by EndTime desc
 select * from shopify.ShopifySyncReports where merchantid = 2798
 
 select * from shopify.shopifyProducts where merchantid = 2798
+
+--Logs
 select top 100 * from tailbasify.dbo.logs
 where merchantid = 2798
 order by id desc
 
 select * from SyncStatus
 
-
-
-select  * from DevTailbaseCore.dbo.TaApplianceEpassParentProducts
-select  * from DevTailbaseCore.dbo.TaApplianceEpassChildProducts
-select * from shopify.shopifyProducts where merchantid = 2887
