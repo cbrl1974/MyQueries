64e9c2f974501b8148e93d278a359e75a781d3ae
cbrl1974 <lcabral@tailbase.com>
2022-03-11 07:28:51 -0800

    Relocate query

---
+++ GTM & GA/update availableInMerchantBean for google analytics scripts.sql
@@ -0,0 +1,13 @@
+DECLARE @MerchantID as int = 3383;
+
+select  merchant_id, textcode, content_1, availableInMerchantBean from MerchantWebsiteTexts where merchant_id = @MerchantID and textcode in ('googleanalyticscode', 'googleanalyticscodeHead')
+
+update top (2) MerchantWebsiteTexts
+set availableinmerchantbean = 1
+where id in (
+    select id from MerchantWebsiteTexts where merchant_id = @MerchantID and textcode in ('googleanalyticscode', 'googleanalyticscodeHead')
+)
+and merchant_id = @MerchantID
+
+
+select  merchant_id, textcode, content_1, availableInMerchantBean from MerchantWebsiteTexts where merchant_id = @MerchantID and textcode in ('googleanalyticscode', 'googleanalyticscodeHead')
