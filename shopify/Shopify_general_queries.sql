27e421a5756628aaaa9fa5eefc1f965f1f94cfdc
cbrl1974 <lcabral@tailbase.com>
2022-03-16 07:45:11 -0700

    Update query

--- shopify/Shopify_general_queries.sql
+++ shopify/Shopify_general_queries.sql
@@ -2,20 +2,30 @@
 select * from shopify.shopifyMerchants where merchantid = 2798
 select  * from shopify.ShopifyMerchantConfigurations where ShopifyMerchantId = 7
 select * from shopify.ShopifyConfigurations order by id
+select * from shopify.ShopifyMerchantCollections where merchantid = 2798
+select * from shopify.shopifyProducts where merchantid = 2798
+--Count
+select count(tailbaseID) from shopify.ShopifyMerchantCollections where merchantid = 2798
+select count(collectionID) from datatail20130410.dbo.merchantCollections where merchant_id = 2798
+select count(tailbaseID) from shopify.shopifyProducts where merchantid = 2798
+select count(productid) from datatail20130410.dbo.merchantProds where merchant_id = 2798
+
+select top 10 * from shopify.ShopifyCollectionRuleInputs
+select top 10 * from shopify.ShopifyMerchantCollectionImages 
 
 --Configs
 select  sc.id, c.Configuration, sc.ConfigurationValue from shopify.ShopifyMerchantConfigurations sc
 inner join shopify.ShopifyConfigurations c on c.Id = sc.ShopifyConfigurationId
 where ShopifyMerchantId = 7
 
-select * from shopify.shopifyProducts where merchantid = 2798
+
 
 --Exports & Reports
-select * from MerchantExports where merchantid = 2798
-
-update top (1) MerchantExports
-set [status] = 1
-where merchantid = 2798
+select * from MerchantExports
+ 
+-- update top (1) MerchantExports
+-- set [status] = 1
+-- where merchantid = 2798
 
 select * from shopify.ShopifyConvertReports where merchantid = 2798 order by EndTime desc
 select * from shopify.ShopifySyncReports where merchantid = 2798
@@ -23,9 +33,11 @@ select * from shopify.ShopifySyncReports where merchantid = 2798
 select * from shopify.shopifyProducts where merchantid = 2798
 
 --Logs
-select top 100 * from tailbasify.dbo.logs
-where merchantid = 2798
+select top 100 * from logs
+-- where merchantid = 1175
 order by id desc
+
+
 
 select * from SyncStatus
 
