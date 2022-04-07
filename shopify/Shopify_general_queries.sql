ca794dc9a2bdcc87ff3308d4e6f90249625f12df
cbrl1974 <lcabral@tailbase.com>
2022-04-07 11:29:32 -0700

    update query

--- shopify/Shopify_general_queries.sql
+++ shopify/Shopify_general_queries.sql
@@ -1,22 +1,41 @@
 -- Merchants
-select * from shopify.shopifyMerchants where merchantid = 2798
-select  * from shopify.ShopifyMerchantConfigurations where ShopifyMerchantId = 7
+select * from shopify.shopifyMerchants where merchantid = 2217
+select  * from shopify.ShopifyMerchantConfigurations where ShopifyMerchantId = 9
 select * from shopify.ShopifyConfigurations order by id
-select * from shopify.ShopifyMerchantCollections where merchantid = 2798
-select * from shopify.shopifyProducts where merchantid = 2798
+select * from shopify.ShopifyMerchantCollections where merchantid = 2217
+select * from shopify.shopifyProducts where merchantid = 2217
+
+
+select  * from shopify.ShopifyMerchantConfigurations where ShopifyMerchantId = 9
+
+ 
+
+
+
 --Count
-select count(tailbaseID) from shopify.ShopifyMerchantCollections where merchantid = 2798
-select count(collectionID) from datatail20130410.dbo.merchantCollections where merchant_id = 2798
-select count(tailbaseID) from shopify.shopifyProducts where merchantid = 2798
-select count(productid) from datatail20130410.dbo.merchantProds where merchant_id = 2798
+select count(1) from shopify.ShopifyMerchantCollections where merchantid = 2217 -- 0
+
+select count(1) from shopify.shopifyProducts where merchantid = 2217  --7947
+select count(1) from shopify.ShopifyProductVariants where ShopifyProductID  in (select id from shopify.shopifyProducts where merchantid = 2217) --7939
+select count(1) from shopify.ShopifyMerchantMetafields where ShopifyProductId  in (select id from shopify.shopifyProducts where merchantid = 2217) --1681
+
+
+select count(1) from datatail20130410.dbo.merchantProds where merchant_id = 2217 --7637
+select count(1) from datatail20130410.dbo.merchantCollections where merchant_id = 2217 --270
+select count(1) from datatail20130410.dbo.merchantRebates where merchant_id = 2217 -- 92
+
+
 
 select top 10 * from shopify.ShopifyCollectionRuleInputs
 select top 10 * from shopify.ShopifyMerchantCollectionImages 
+select * from SyncStatus
 
 --Configs
-select  sc.id, c.Configuration, sc.ConfigurationValue from shopify.ShopifyMerchantConfigurations sc
+select  c.id, c.Configuration, sc.ConfigurationValue from shopify.ShopifyMerchantConfigurations sc
 inner join shopify.ShopifyConfigurations c on c.Id = sc.ShopifyConfigurationId
-where ShopifyMerchantId = 7
+where ShopifyMerchantId = 9
+select  * from shopify.ShopifyMerchantConfigurations where ShopifyMerchantId = 9
+
 
 
 
@@ -25,19 +44,16 @@ select * from MerchantExports
  
 -- update top (1) MerchantExports
 -- set [status] = 1
--- where merchantid = 2798
+-- where merchantid = 2217
 
-select * from shopify.ShopifyConvertReports where merchantid = 2798 order by EndTime desc
-select * from shopify.ShopifySyncReports where merchantid = 2798
+select * from shopify.ShopifyConvertReports where merchantid = 2217 order by EndTime desc
+select * from shopify.ShopifySyncReports where merchantid = 2217
 
-select * from shopify.shopifyProducts where merchantid = 2798
+select * from shopify.shopifyProducts where merchantid = 2217
 
 --Logs
-select top 100 * from logs
--- where merchantid = 1175
+select top 500 * from logs
 order by id desc
 
 
-
-select * from SyncStatus
 
