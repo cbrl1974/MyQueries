e649c8196e2cab97dc0c3a712d71a97b46e37dd6
cbrl1974 <lcabral@tailbase.com>
2022-05-04 13:07:39 -0700

    Update query

--- Tailbasify/Shopify_general_queries.sql
+++ Tailbasify/Shopify_general_queries.sql
@@ -1,62 +1,64 @@
 
---Exports & Reports
-select * from MerchantExports
- 
--- update top (1) MerchantExports
--- set [status] = 1
--- where merchantid = 3336
-
-
---Logs
-select top 100 * from logs
---where text like '%583771%'
-order by id desc
-
 
 -- Merchants
-select * from shopify.shopifyMerchants where merchantid = 3336
+select * from shopify.shopifyMerchants where merchantid = 1202
 
---Collections
-select * from shopify.ShopifyMerchantCollections where merchantid = 3336
+-- update top (1) MerchantExports
+-- set [status] = 1
+-- where merchantid = 3209
 
---Products
-select * from shopify.shopifyProducts where merchantid = 3336 and tailbaseid = 583771
+select * from MerchantExports
+
+select top 100 * from logs  WITH (NOLOCK)
+WHERE  merchantid = 1202
+order by LogTime desc
+
+--Reports
+select * from shopify.ShopifyConvertReports where merchantid = 1202 order by EndTime desc
+select * from shopify.ShopifySyncReports where merchantid = 1202  order by EndTime desc
+
+
+select top(50) *
+from
+    Shopify.ShopifySyncReports h
+join
+    Shopify.ShopifyProductSyncReportsDetail d on h.Id = d.SyncReportId
+where
+    d.ShopifyProductId in (select Id from Shopify.ShopifyProducts where MerchantId = 1202)
+order by
+    h.id desc
+
+
+select * from shopify.ShopifyMerchantMetafields where ShopifyProductId  in (select id from shopify.shopifyProducts where merchantid = 1202)
+ and  keyname = 'BundlesOfProduct'
 
 
 
 
 --Count on tailbasify
-select count(1) from shopify.ShopifyMerchantCollections where merchantid = 3336 -- 173
-select count(1) from shopify.shopifyProducts where merchantid = 3336  --7947
-select count(1) from shopify.ShopifyProductVariants where ShopifyProductID  in (select id from shopify.shopifyProducts where merchantid = 3336) --7939
-select count(1) from shopify.ShopifyMerchantMetafields where ShopifyProductId  in (select id from shopify.shopifyProducts where merchantid = 3336) --1681
-
---Count on Production
-select count(1) from [pvm701].datatail20130410.dbo.merchantProds where merchant_id = 3336 --7637
-select count(1) from [pvm701].datatail20130410.dbo.merchantCollections where merchant_id = 3336 --270
-select count(1) from [pvm701].datatail20130410.dbo.merchantRebates where merchant_id = 3336 -- 92
+select count(1) from shopify.ShopifyMerchantCollections where merchantid = 1202
+select * from shopify.shopifyProducts where merchantid = 1202 and ShopifyGeneratedProductId is not null
+select count(1) from shopify.ShopifyProductVariants where ShopifyProductID  in (select id from shopify.shopifyProducts where merchantid = 1202)
+select count(1) from shopify.ShopifyMerchantMetafields where ShopifyProductId  in (select id from shopify.shopifyProducts where merchantid = 1202)
 
 
 
 --Configs
 select  c.id, c.Configuration, sc.ConfigurationValue from shopify.ShopifyMerchantConfigurations sc
 inner join shopify.ShopifyConfigurations c on c.Id = sc.ShopifyConfigurationId
-where ShopifyMerchantId = 9
-select  * from shopify.ShopifyMerchantConfigurations where ShopifyMerchantId = 9
+where ShopifyMerchantId = 12
 
 
 select  * from shopify.ShopifyMerchantConfigurations where ShopifyMerchantId = 9
 select * from shopify.ShopifyConfigurations order by id
 
---Reports
-select * from shopify.ShopifyConvertReports where merchantid = 3336 order by EndTime desc
-select * from shopify.ShopifySyncReports where merchantid = 3336
+
 
 -- Other tables on Tailbasify
 select top 10 * from shopify.ShopifyCollectionRuleInputs
 select top 10 * from shopify.ShopifyMerchantCollectionImages 
 select * from SyncStatus
-select * from shopify.shopifyProducts where merchantid = 3336
+select * from shopify.shopifyProducts where merchantid = 1448
 
 
 
