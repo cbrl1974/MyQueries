ece5bd8328f1d53c0af6aa895622979885e1be66
cbrl1974 <lcabral@tailbase.com>
2022-09-23 06:07:02 -0700

    Udates

--- Tailbasify/Shopify_general_queries.sql
+++ Tailbasify/Shopify_general_queries.sql
@@ -3,16 +3,18 @@
 -- Merchants
 select s.id, m.merchant, s.MerchantId, s.ShopUrl, s.SecurityStamp, s.ApiVersion
 from shopify.shopifyMerchants s
-    inner join datatail20130410.dbo.merchants m on m.id = s.MerchantId
+    inner join devTailbaseCore.dbo.merchants m on m.id = s.MerchantId
 where m.id = 1202
 
-update top (1) MerchantExports
-set [status] = 1
-where merchantid = 2798
+select count(productid) from devTailbaseCore.dbo.merchantProds where merchant_id = 1202
+
+-- update top (1) MerchantExports
+-- set [status] = 1
+-- where merchantid = 1202
 
 select  m.merchant, me.*
 from MerchantExports me
- inner join datatail20130410.dbo.merchants m on m.id = me.MerchantId
+ inner join devTailbaseCore.dbo.merchants m on m.id = me.MerchantId
 order by [Status] desc
 
 
@@ -24,7 +26,9 @@ WHERE  merchantid = 1202
     AND module = 'Converter'
 order by LogTime desc
 
-
+SELECT *
+  FROM [Tailbasify].[Shopify].[ShopifyProducts]
+  where TailbaseId = 744424
 
 select
     top(10)
@@ -32,7 +36,7 @@ select
 from Shopify.ShopifyConvertReports h WITH (NOLOCK)
     join Shopify.ShopifyConvertProductReportsDetail d on h.id = d.ConvertReportId
 where h.MerchantId = 1202
-    and d.TailbaseId in (628629)
+    and d.TailbaseId in (744424)
 order by h.id desc
 
 
@@ -47,16 +51,12 @@ order by id desc
 
 --Sunc Report
 select
-    --distinct d.[Status]
     top(10)
     *
 from Shopify.ShopifySyncReports h WITH (NOLOCK)
     join Shopify.ShopifyProductSyncReportsDetail d on h.id = d.SyncReportId
 where h.MerchantId = 1202
-    and d.ShopifyProductId in (626050)
---and status = 6
---623004
---group by  d.[Status]
+    and d.ShopifyProductId in (639104)
 order by h.id desc
 
 select *
@@ -65,27 +65,30 @@ where ShopifyProductId IN ( 623004,358235)
 
 
 select *
-from datatail20130410.dbo.merchantProds
+from devTailbaseCore.dbo.merchantProds
 where merchant_id = 1202 and
 
 
 select *
-from datatail20130410.dbo.merchantprods
-where merchant_id = 1202 and productid in (628629)
+from devTailbaseCore.dbo.merchantprods
+where merchant_id = 1202
+ and productid in (628629)
 
 select *
-from datatail20130410.dbo.merchantCollections
+from devTailbaseCore.dbo.merchantCollections
 where merchant_id = 1202 and collectionID = 22771
 
 select *
-from datatail20130410.dbo.merchantcats
+from devTailbaseCore.dbo.merchantcats
 where merchant_id = 1202
 
 
 
-select *
+select tailbaseid, TitleEn, TitleFr
 from shopify.shopifyProducts
-where merchantid = 1202 and tailbaseid  in (628629)
+where merchantid = 1202 
+and tailbaseid  in (494842, 623406, 494851)
+
 
 select *
 from shopify.ShopifyMerchantCollections
@@ -148,9 +151,9 @@ select *
 from shopify.ShopifyCollectionRuleInputs
 where ShopifyMerchantCollectionId = 6890
 --First Delete Rule
-delete top (1)  from shopify.ShopifyCollectionRuleInputs where ShopifyMerchantCollectionId =6890
---Second Delete ShopifyMerchantCollections
-delete top (1)  from shopify.ShopifyMerchantCollections where MerchantId = 1202 and id = 6890
+-- delete top (1)  from shopify.ShopifyCollectionRuleInputs where ShopifyMerchantCollectionId =6890
+-- --Second Delete ShopifyMerchantCollections
+-- delete top (1)  from shopify.ShopifyMerchantCollections where MerchantId = 1202 and id = 6890
 
 select * from Shopify.ShopifyMediaContentTypes
 
--- Tracktail/get Tracktail app users.sql
+++ Tracktail/get Tracktail app users.sql
@@ -1,87 +1,111 @@
 
-SELECT * FROM [TrackTailApp].[AspNetUsers] where Email = 'lcabral@tailbase.com';
-SELECT * FROM [TrackTailApp].[AspNetUsers] where Email like '%cityfurniture%'
+SELECT *
+FROM [TrackTailApp].[AspNetUsers]
+where Email = 'lcabral@tailbase.com';
 
-SELECT * FROM [TrackTailApp].ClientAccounts where merchantid = 1202
+SELECT *
+FROM [TrackTailApp].AspNetRoles
 
-SELECT * FROM [TrackTailApp].ClientAccounts  where AccountId in (525,224)
+
+SELECT *
+FROM [TrackTailApp].ClientAccounts
+where merchantid = 1202
 
 update top (4) [TrackTailApp].[AspNetUsers]
 set ClientId = 224
 where Email like '%cityfurniture%'
 
-SELECT * FROM [TrackTailApp].[AspNetUsers] where Email like '%tailbase%';
-SELECT * FROM [TrackTailApp].ClientAccounts where merchantid = 1202
-SELECT * FROM [TrackTailApp].ClientAccounts where merchantid = 589
-SELECT top 100  * FROM TrackTailDb.Tracktail_Field where merchantid = 1202
-SELECT top 100  * FROM TrackTailDb.Tracktail_Field where merchantid = 589
+SELECT *
+FROM [TrackTailApp].[AspNetUsers]
+where Email like '%tailbase%';
 
-SELECT top 100 *  from  TrackTailSolutions_Clients where merchantid = 1448
+SELECT *
+FROM [TrackTailApp].ClientAccounts
+where merchantid = 1202
 
-update top (1) TrackTailSolutions_Clients
-set CountryCode = 'CA'
-where merchantid = 1448 
-SELECT * FROM [TrackTailApp].[AspNetRoles]
+SELECT *
+FROM [TrackTailApp].ClientAccounts
+where merchantid = 589
 
-SELECT u.UserName, r.Name FROM [TrackTailApp].[aspnetuserroles] ur
-inner join [TrackTailApp].[AspNetRoles] r on r.Id = ur.RoleId
-inner join [TrackTailApp].[AspNetUsers] u on u.id = ur.UserId
+SELECT top 100
+    *
+FROM TrackTailDb.Tracktail_Field
+where merchantid = 1202
+
+
+SELECT top 100
+    *
+from TrackTailSolutions_Clients
+where merchantid = 1448
+
+-- update top (1) TrackTailSolutions_Clients
+-- set CountryCode = 'CA'
+-- where merchantid = 1448 
+-- SELECT * FROM [TrackTailApp].[AspNetRoles]
+
+SELECT u.UserName, r.Name
+FROM [TrackTailApp].[aspnetuserroles] ur
+    inner join [TrackTailApp].[AspNetRoles] r on r.Id = ur.RoleId
+    inner join [TrackTailApp].[AspNetUsers] u on u.id = ur.UserId
 where UserId in ('61d59b42-3f86-4ca3-9ddb-3f22774e19ae','6455eae3-3382-4dd7-ac57-f8eb1abc18a3','c52fa2d8-4e9b-4304-af5b-a1ab9b3a87ff','ffd95ac0-7f82-4d6e-9f66-70b2a32f23f6')
 
 
 
 
-SELECT * FROM  [datatail20130410].[TrackTailDb].[Websites] order by id
+SELECT *
+FROM [datatail20130410].[TrackTailDb].[Websites]
+order by id
 
 
-sELECT coalesce(max(r.AdminLevel),0) adminlevel FROM TrackTailApp.AspNetUserRoles ur
-                    left join TrackTailApp.AspNetRoles r on r.id=ur.RoleId
-                    where ur.UserId='b1ec3b6a-2c45-4cb6-a708-5688e56621a9'
+sELECT coalesce(max(r.AdminLevel),0) adminlevel
+FROM TrackTailApp.AspNetUserRoles ur
+    left join TrackTailApp.AspNetRoles r on r.id=ur.RoleId
+where ur.UserId='b1ec3b6a-2c45-4cb6-a708-5688e56621a9'
 
-SELECT TOP (10) [LogID]
-      ,[Project]
-      ,[Category]
-      ,[Text]
-      ,[Time]
-      ,[Severity]
-      ,[Module]
-      ,[Location]
-  FROM [EventReactor].[dbo].[Logs]
-  where category = 'tracktail'
-  order by [time] desc
+SELECT TOP (10)
+    [LogID]
+      , [Project]
+      , [Category]
+      , [Text]
+      , [Time]
+      , [Severity]
+      , [Module]
+      , [Location]
+FROM [EventReactor].[dbo].[Logs]
+where category = 'tracktail'
+order by [time] desc
 
 SELECT TCA.[ID]
-                            , TCA.[MerchantId]
-                            , DDC.cie
-                            , TCA.[CieId]
-                            , TCA.[ProvStateId]
-                            , TCA.[CountryCode]
-                            , TCA.[Langue]
-                            , TCA.[Active]
-                            , DDM.merchant
-                            , DDC.website
-                            , (
-                                SELECT CASE
-                        
-                                        WHEN EXISTS(
-                                                SELECT TOP 1[id]
-                        
-                                                FROM MerchantBrands
-                        
-                                                WHERE merchant_id = TCA.MerchantId
-                                                )
-                        
-                                            THEN CAST(1 AS BIT)
-                        
-                                        ELSE CAST(0 AS BIT)
-                        
-                                        END
-                                ) AS 'HasWebsite', 
-                                (select top 1 brand from merchantFeeds where merchant_id = TCA.[MerchantId] and brand = 'NECO_NE' and active = 1) AS 'NECO_NE_brand',
-                                (select top 1 brand from MerchantFeedWarehouses where merchant_id = TCA.[MerchantId] and active = 1 and brand = 'ADC') AS 'NECO_ADC_brand'
-
-                        FROM TrackTailSolutions_Clients TCA
-                        JOIN merchants DDM ON DDM.ID = TCA.MerchantId
-                        JOIN Companies DDC ON DDC.Id_cie = TCA.CieId
-                        WHERE TCA.[CountryCode] is null
-                        --WHERE TCA.ID = 483
+    , TCA.[MerchantId]
+    , DDC.cie
+    , TCA.[CieId]
+    , TCA.[ProvStateId]
+    , TCA.[CountryCode]
+    , TCA.[Langue]
+    , TCA.[Active]
+    , DDM.merchant
+    , DDC.website
+    , (
+        SELECT CASE
+                WHEN EXISTS(
+                        SELECT TOP 1
+            [id]
+        FROM MerchantBrands
+        WHERE merchant_id = TCA.MerchantId
+                        )
+                    THEN CAST(1 AS BIT)
+                ELSE CAST(0 AS BIT)
+                END
+        ) AS 'HasWebsite',
+    (select top 1
+        brand
+    from merchantFeeds
+    where merchant_id = TCA.[MerchantId] and brand = 'NECO_NE' and active = 1) AS 'NECO_NE_brand',
+    (select top 1
+        brand
+    from MerchantFeedWarehouses
+    where merchant_id = TCA.[MerchantId] and active = 1 and brand = 'ADC') AS 'NECO_ADC_brand'
+FROM TrackTailSolutions_Clients TCA
+    JOIN merchants DDM ON DDM.ID = TCA.MerchantId
+    JOIN Companies DDC ON DDC.Id_cie = TCA.CieId
+WHERE TCA.ID = 483
