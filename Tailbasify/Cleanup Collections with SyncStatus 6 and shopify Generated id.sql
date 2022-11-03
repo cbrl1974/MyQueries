ff0a7b2445dd90e287cfe0ef4d5e39af3229241e
cbrl1974 <cbrl1974@gmail.com>
2022-11-03 16:35:46 -0700

    update

--- Tailbasify/Cleanup Collections with SyncStatus 6 and shopify Generated id.sql
+++ Tailbasify/Cleanup Collections with SyncStatus 6 and shopify Generated id.sql
@@ -1,34 +1,40 @@
 select s.id, m.merchant, s.MerchantId, s.ShopUrl, s.SecurityStamp, s.ApiVersion
-from devtailbasify.shopify.shopifyMerchants s
+from Tailbasify.shopify.shopifyMerchants s
     inner join merchants m on m.id = s.MerchantId
 where m.id in (1956)
 
 
-select  m.merchant, me.*
-from devtailbasify.dbo.MerchantExports me
- inner join merchants m on m.id = me.MerchantId
-order by [Status] desc
-
 --Checking by targetting some collection rebates
 SELECT  *
-FROM devtailbasify.shopify.ShopifyMerchantCollections
-WHERE MerchantId = 1956
-AND tailbaseID IN ( SELECT TailbaseId FROM devtailbasify.shopify.ShopifyProducts WHERE merchantid = 1956 AND itemtype = 3)
+FROM Tailbasify.shopify.ShopifyMerchantCollections
+WHERE MerchantId = 3209
+AND tailbaseID IN ( 
+    SELECT TailbaseId 
+    FROM Tailbasify.shopify.ShopifyProducts 
+    WHERE merchantid = 3209 
+    AND itemtype = 3
+    )
 and SyncStatusId  = 6
 and ShopifyGeneratedCollectionId is null
 
---Case test: id: 11237; Title:Fisher Paykel 5 Year Warranty Offer had a syncstatus 6!
+
 
 
 --Checking all collections 
 SELECT  *
-FROM devtailbasify.shopify.ShopifyMerchantCollections mc
+FROM Tailbasify.shopify.ShopifyMerchantCollections mc
 where SyncStatusId  = 6
-and MerchantId = 1956
 and ShopifyGeneratedCollectionId is null
+and merchantid = 3096 
+
+update top (1) Tailbasify.shopify.ShopifyMerchantCollections
+set SyncStatusId = 1
+where MerchantId = 1956
+and id = 11237
+
 
 --
-update top (172) devtailbasify.shopify.ShopifyMerchantCollections
+update top (172) Tailbasify.shopify.ShopifyMerchantCollections
 set SyncStatusId = 1
 where SyncStatusId  = 6
 and MerchantId = 1956
@@ -37,21 +43,11 @@ and ShopifyGeneratedCollectionId is null
 
 
 --First Delete Rule
-delete from devtailbasify.shopify.ShopifyCollectionRuleInputs
-where ShopifyMerchantCollectionId in ( 
-    SELECT  id
-    FROM devtailbasify.shopify.ShopifyMerchantCollections mc
-    where SyncStatusId  = 6
-    and MerchantId = 1956
-    and ShopifyGeneratedCollectionId is null
-)
+delete from Tailbasify.shopify.ShopifyCollectionRuleInputs
+where ShopifyMerchantCollectionId in (8195,7864,7867,7881,7878,7871,7870,7868,7873,7874,7866,7876,7875,7872,7880,8194,7865,7879,7877)
+
 
 -- Second Delete ShopifyMerchantCollections
-delete from devtailbasify.shopify.ShopifyMerchantCollections
-    where MerchantId = 1956 
-    and id in (SELECT id
-    FROM devtailbasify.shopify.ShopifyMerchantCollections mc
-    where SyncStatusId  = 6
-    and MerchantId = 1956
-    and ShopifyGeneratedCollectionId is null
-)
+delete from Tailbasify.shopify.ShopifyMerchantCollections
+where id in (8195,7864,7867,7881,7878,7871,7870,7868,7873,7874,7866,7876,7875,7872,7880,8194,7865,7879,7877)
+
