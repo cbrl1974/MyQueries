0cee6991c32b58b69a7827b438e3d17f6d005299
cbrl1974 <cbrl1974@gmail.com>
2023-02-03 08:53:56 -0800

    Updates

--- Tailbasify/Queries to onboard Merchant DB.sql
+++ Tailbasify/Queries to onboard Merchant DB.sql
@@ -1,27 +1,39 @@
+use tailbasify
+DECLARE @MerchantId AS INT = 3039;
+DECLARE @NewShopifyMerchantId AS INT;
+DECLARE @shopifyMerchantUrl AS varchar(max) = 'handy-appliances-bc.myshopify.com';
+DECLARE @TailbasifyAppURL AS varchar(max) = 'hdyapp.tailbasify.com';
+DECLARE @shopifyMerchantApiVersion AS varchar(10) = 'v2';
+DECLARE @ShopifyApiKey as nvarchar(max) = 'ab06be99152c8eeab3460f874b9920b1'
+DECLARE @ShopifySecretKey as nvarchar(max) = 'f720e882b7955df87c74673589302518'
+
+-- Links for the app installlation
+--https://hdyapp.tailbasify.com/v2/api/shopify/install
+--https://hdyapp.tailbasify.com/v2/api/Shopify/Authorize
+
+--Insert New Store
+--INSERT INTO [Shopify].[ShopifyMerchants](MERCHANTID, SHOPURL, LIVE, SECURITYSTAMP, CREATIONDATE, MODIFICATIONDATE, APIKEY, SecretKey,APPURL, APIVersion)
+--VALUES (@MerchantId, @shopifyMerchantUrl, 0, NEWID(), GETDATE(), GETDATE(), @ShopifyApiKey, @ShopifySecretKey, @TailbasifyAppURL, @shopifyMerchantApiVersion)
+--SET @NewShopifyMerchantId = @@IDENTITY
+
+--Add Record to Merchant Exports Table
+--INSERT INTO [dbo].[MerchantExports] (MERCHANTID, status, ShopifyMerchantId)
+--VALUES (@MerchantId, 1, @NewShopifyMerchantId)
+
 -- Merchants
 select 
 m.merchant, s.*
 from shopify.shopifyMerchants s
     inner join datatail20130410.dbo.merchants m on m.id = s.MerchantId
-where m.id = 3443
-
---Insert New Store
---INSERT INTO [Shopify].[ShopifyMerchants](MERCHANTID, SHOPURL, LIVE, SECURITYSTAMP, CREATIONDATE, MODIFICATIONDATE, APIKEY, SecretKey,APPURL, APIVersion)
---VALUES (3443, 'lux-by-walo.myshopify.com', 0, NEWID(), GETDATE(), GETDATE(), '5814a9b3795b318f76c562f9370a3694', '9058e7faf4495aa45515a981f0eecaf4','lxbywa.tailbasify.com','v2')
-
---Add Record to Merchant Exports Table
---INSERT INTO [dbo].[MerchantExports] (MERCHANTID, status, ShopifyMerchantId)
---VALUES (3443, 1, 33)
+where m.id = @MerchantId
 
 --Check new entry
 select * from [dbo].[MerchantExports]
 
---Add configs
+--********** configs
+
 --Get example
-select c.id, c.Configuration, sc.ConfigurationValue
-from shopify.ShopifyMerchantConfigurations sc
-    inner join shopify.ShopifyConfigurations c on c.Id = sc.ShopifyConfigurationId
-where ShopifyMerchantId = 33
+DECLARE @NewShopifyMerchantId AS INT = 34;
 
 select *
 from shopify.ShopifyConfigurations
@@ -29,8 +41,15 @@ order by id
 
 --Add Config Record
 insert into shopify.ShopifyMerchantConfigurations (shopifyMerchantid, ShopifyConfigurationId, ConfigurationValue) values
-(33,4,'English'), -- PrimaryLanguage
-(33,8,'True')  -- ProceQualifier
+(@NewShopifyMerchantId,4,'English'), -- PrimaryLanguage
+(@NewShopifyMerchantId,8,'True'),  -- PriceQualifier
+(@NewShopifyMerchantId,3,'True')  -- AssociatedProducts
+
+--Check Entry
+select c.id, c.Configuration, sc.ConfigurationValue
+from shopify.ShopifyMerchantConfigurations sc
+    inner join shopify.ShopifyConfigurations c on c.Id = sc.ShopifyConfigurationId
+where ShopifyMerchantId = @NewShopifyMerchantId
 
 -- ***********All configs available******************
 
