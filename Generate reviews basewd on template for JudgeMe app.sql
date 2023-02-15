d0e92a9fa4a80abfd59bb2fef4012de0c6fe1b09
cbrl1974 <cbrl1974@gmail.com>
2023-02-15 10:49:11 -0800

    Update

--- Generate reviews basewd on template for JudgeMe app.sql
+++ Generate reviews basewd on template for JudgeMe app.sql
@@ -1,8 +1,17 @@
 DECLARE @MerchantID as int = 1911;
-DECLARE @Total as int = 2000;
+DECLARE @TotalLimit as int = 5;
+-- 
+DECLARE @ReviewsCountOnThisMerchant as int =  (SELECT count(distinct r.ReviewTitle) FROM Reviews r
+INNER JOIN ReviewsUsers ru ON r.ReviewsUserID = ru.id
+INNER JOIN [dbo].[fn_GetMerchantProductsWithCollectionProducts](@MerchantID) prods ON prods.productid = r.ItemID
+INNER JOIN Merchants m ON m.ID = prods.MerchantId
+INNER JOIN tailbasify.shopify.shopifyProducts s on s.tailbaseid = prods.productid and s.itemtype = 1
+WHERE r.Id_langue = 1);
+
+select @ReviewsCountOnThisMerchant
 
 SELECT  distinct 
-    top (@Total) --limits the download time. Remove or comment this line if you all
+    top (@TotalLimit) --limits the download time. Remove or comment this line if you all
      r.ReviewTitle as 'title',
     r.ReviewText as 'body',
     r.OverallRating AS rating,    
