ba4830ce53af400ad8ed0f7ffe5d323bd32aca58
cbrl1974 <cbrl1974@gmail.com>
2023-05-31 15:48:40 -0400

    Update

--- Tailbasify/Cleanup Collections with SyncStatus 6 and shopify Generated id.sql
+++ Tailbasify/Cleanup Collections with SyncStatus 6 and shopify Generated id.sql
@@ -1,48 +1,19 @@
-select s.id, m.merchant, s.MerchantId, s.ShopUrl, s.SecurityStamp, s.ApiVersion
-from Tailbasify.shopify.shopifyMerchants s
-    inner join merchants m on m.id = s.MerchantId
-where m.id in (1956)
+DECLARE @count INT;
 
-
---Checking by targetting some collection rebates
-SELECT  *
-FROM Tailbasify.shopify.ShopifyMerchantCollections
-WHERE MerchantId = 3209
-AND tailbaseID IN ( 
-    SELECT TailbaseId 
-    FROM Tailbasify.shopify.ShopifyProducts 
-    WHERE merchantid = 3209 
-    AND itemtype = 3
-    )
-and SyncStatusId  = 6
+select @count = COUNT(*) from shopify.ShopifyMerchantCollections  where  SyncStatusId  = 6
 and ShopifyGeneratedCollectionId is null
 
+select @count
 
+IF @count > 0
+	BEGIN
+		delete from shopify.ShopifyCollectionRuleInputs where ShopifyMerchantCollectionId in 
+		(
+			select id from shopify.ShopifyMerchantCollections  where  SyncStatusId  = 6
+		and ShopifyGeneratedCollectionId is null
+		)
 
-
---Checking all collections 
-SELECT  *
-FROM Tailbasify.shopify.ShopifyMerchantCollections mc
-where SyncStatusId  = 6
-and ShopifyGeneratedCollectionId is null
-and merchantid = 1956 
-
-
-
---First Delete Rule
-delete from Tailbasify.shopify.ShopifyCollectionRuleInputs
-where ShopifyMerchantCollectionId in (
-            SELECT  id
-        FROM Tailbasify.shopify.ShopifyMerchantCollections mc
-        where SyncStatusId  = 6
-        and ShopifyGeneratedCollectionId is null
-        and merchantid = 1956 
-    )
-
-
--- Second Delete ShopifyMerchantCollections
-delete from Tailbasify.shopify.ShopifyMerchantCollections
-where  SyncStatusId  = 6
-and ShopifyGeneratedCollectionId is null
-and merchantid = 1956 
-
+		delete from shopify.ShopifyMerchantCollections
+		where  SyncStatusId  = 6
+		and ShopifyGeneratedCollectionId is null
+	END
