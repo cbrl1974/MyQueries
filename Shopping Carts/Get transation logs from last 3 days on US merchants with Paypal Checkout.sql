984be9dc0ac7978f4654834a3dcf7f8e490d6996
cbrl1974 <cbrl1974@gmail.com>
2023-06-15 19:43:46 -0400

    Update

---
+++ Shopping Carts/Get transation logs from last 3 days on US merchants with Paypal Checkout.sql
@@ -0,0 +1,17 @@
+
+select top 150  * from MerchantTransactionLog
+where merchant_id in (
+	select m.id
+	from merchantwebsitefeatures mf
+	join merchants m on m.id = mf.merchant_id
+	where  mf.featureCode = 'shoppingCartType'
+	and  mf.featurevalue like '%onlyppc%'
+	and m.active = 1
+	and  mf.merchant_id in (select merchant_id  from merchantwebsitefeatures
+	where featurevalue    in ('tbplatinum') and featureCode = 'template')
+	and m.countryCode = 'US'
+	)
+    AND creationTimestamp >= DATEADD(DAY, -3, GETDATE())
+	and txnservice not like '%PayPal Checkout Order Transaction SANDBOX TEST%'
+order by id desc
+
