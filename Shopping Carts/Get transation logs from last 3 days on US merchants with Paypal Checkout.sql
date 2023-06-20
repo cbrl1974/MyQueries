02d6ed5458991c59b2ee991db06cb94a55dc884f
cbrl1974 <cbrl1974@gmail.com>
2023-06-20 09:21:58 -0400

    Update

--- Shopping Carts/Get transation logs from last 3 days on US merchants with Paypal Checkout.sql
+++ Shopping Carts/Get transation logs from last 3 days on US merchants with Paypal Checkout.sql
@@ -1,5 +1,5 @@
 
-select top 150  * from MerchantTransactionLog
+select * from MerchantTransactionLog
 where merchant_id in (
 	select m.id
 	from merchantwebsitefeatures mf
@@ -13,5 +13,19 @@ where merchant_id in (
 	)
     AND creationTimestamp >= DATEADD(DAY, -3, GETDATE())
 	and txnservice not like '%PayPal Checkout Order Transaction SANDBOX TEST%'
+	union
+	select * from MerchantTransactionLog
+where merchant_id in (
+	select m.id
+	from merchantwebsitefeatures mf
+	join merchants m on m.id = mf.merchant_id
+	where  mf.featureCode = 'hasMultiPayment'
+	and  mf.featurevalue like '%onlyppc%'
+	and m.active = 1
+	and  mf.merchant_id in (select merchant_id  from merchantwebsitefeatures
+	where featurevalue    in ('tbplatinum') and featureCode = 'template')
+	and m.countryCode = 'US'
+	)
+    AND creationTimestamp >= DATEADD(DAY, -3, GETDATE())
+	and txnservice not like '%PayPal Checkout Order Transaction SANDBOX TEST%'
 order by id desc
-
