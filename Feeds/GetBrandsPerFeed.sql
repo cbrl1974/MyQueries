bdc3384b58c5cd7ccb936ca1bdb090676c42e66a
cbrl1974 <cbrl1974@gmail.com>
2023-09-19 09:59:08 -0400

    update

--- Feeds/GetBrandsPerFeed.sql
+++ Feeds/GetBrandsPerFeed.sql
@@ -1,36 +1,69 @@
--- Declare a table variable to store the results
-DECLARE @ExtractedResults TABLE (
+-- Declare a table variable to store the combined results
+DECLARE @CombinedResults TABLE (
     ID INT,
     Name NVARCHAR(255),
     Result INT
 );
 
-DECLARE @Id INT;
-DECLARE @Name NVARCHAR(255);
-DECLARE @Options NVARCHAR(MAX);
+-- Query 1
+DECLARE @Id1 INT;
+DECLARE @Name1 NVARCHAR(255);
+DECLARE @Options1 NVARCHAR(MAX);
 
-DECLARE DataCursor CURSOR FOR
+DECLARE DataCursor1 CURSOR FOR
 SELECT id, name, [options]
-FROM feeds.Feeds;
+FROM feeds.Feeds
+WHERE id NOT IN (3, 2, 17, 13, 14);
 
-OPEN DataCursor;
+OPEN DataCursor1;
 
-FETCH NEXT FROM DataCursor INTO @Id, @Name, @Options;
+FETCH NEXT FROM DataCursor1 INTO @Id1, @Name1, @Options1;
 
 WHILE @@FETCH_STATUS = 0
 BEGIN
-    INSERT INTO @ExtractedResults (ID, Name, Result)
+    INSERT INTO @CombinedResults (ID, Name, Result)
     SELECT
-        @Id AS 'ID',
-        @Name AS 'Name',
+        @Id1 AS 'ID',
+        @Name1 AS 'Name',
         CAST(JSON_VALUE(bc.value, '$.brandId') AS INT) AS 'Result'
-    FROM OPENJSON(@Options, '$.brandCategories') AS bc;
+    FROM OPENJSON(@Options1, '$.brandCategories') AS bc
+    WHERE JSON_VALUE(bc.value, '$.brandId') IS NOT NULL;
 
-    FETCH NEXT FROM DataCursor INTO @Id, @Name, @Options;
+    FETCH NEXT FROM DataCursor1 INTO @Id1, @Name1, @Options1;
 END;
 
-CLOSE DataCursor;
-DEALLOCATE DataCursor;
+CLOSE DataCursor1;
+DEALLOCATE DataCursor1;
 
--- Select the results from the table variable
-SELECT DISTINCT * FROM @ExtractedResults;
+-- Query 2
+DECLARE @Id2 INT;
+DECLARE @Name2 NVARCHAR(255);
+DECLARE @Options2 NVARCHAR(MAX);
+
+DECLARE DataCursor2 CURSOR FOR
+SELECT id, name, [options]
+FROM feeds.Feeds
+WHERE id NOT IN (3, 2, 17, 13, 14);
+
+OPEN DataCursor2;
+
+FETCH NEXT FROM DataCursor2 INTO @Id2, @Name2, @Options2;
+
+WHILE @@FETCH_STATUS = 0
+BEGIN
+    INSERT INTO @CombinedResults (ID, Name, Result)
+    SELECT
+        @Id2 AS 'ID',
+        @Name2 AS 'Name',
+        CAST(value AS INT) AS 'Result'
+    FROM OPENJSON(@Options2, '$.statusProcessorSettings.manufIds')
+    WHERE value IS NOT NULL;
+
+    FETCH NEXT FROM DataCursor2 INTO @Id2, @Name2, @Options2;
+END;
+
+CLOSE DataCursor2;
+DEALLOCATE DataCursor2;
+
+-- Select the combined results without NULL values
+SELECT DISTINCT * FROM @CombinedResults WHERE Result IS NOT NULL;
