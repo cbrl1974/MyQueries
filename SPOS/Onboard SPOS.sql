2386f4c78463a61f108d5abbd8f21938343969b2
cbrl1974 <cbrl1974@gmail.com>
2024-02-06 11:46:24 -0500

    New query

---
+++ SPOS/Onboard SPOS.sql
@@ -0,0 +1,14 @@
+﻿Declare @merchantID as int = 1479;
+select * from MerchantPosConfigs where merchantId = @merchantID
+
+INSERT INTO MerchantPosConfigs (MerchantId, ApiKey, LocalPath, [FileName], Config) VALUES
+(
+ @merchantID,
+ NEWID(),
+ '/GermainLariviere/ProductInventory',
+ 'ProductInventory.json',
+ '{"ProcessProducts": true, "ProcessInventory": true, "ProcessMerchantItemTags": false, "ProcessAdditionalInformation": false}'
+)
+
+select * from MerchantPosConfigs where merchantId = @merchantID
+ 
