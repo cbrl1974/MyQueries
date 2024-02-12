ac64008f7a18fc5de12fcf2f1ff9f598193c0889
cbrl1974 <cbrl1974@gmail.com>
2024-02-12 07:22:32 -0500

    Update query

--- Tailbasify/Check configs on merchants.sql
+++ Tailbasify/Check configs on merchants.sql
@@ -1,7 +1,7 @@
 --**************************Configs
-DECLARE @MerchantId as int = 1448;
+DECLARE @MerchantId as int = 3388;
 DECLARE @ShopifyMerchantId as int =  (select s.id from shopify.shopifyMerchants s
-    inner join devTailbaseCore.dbo.merchants m on m.id = s.MerchantId
+    inner join datatail20130410.dbo.merchants m on m.id = s.MerchantId
 where m.id = @MerchantId);
 
 select @ShopifyMerchantId as ShopifyMerchantId, c.id, c.Configuration, sc.ConfigurationValue
@@ -9,18 +9,19 @@ from shopify.ShopifyMerchantConfigurations sc
     inner join shopify.ShopifyConfigurations c on c.Id = sc.ShopifyConfigurationId
 where ShopifyMerchantId = @ShopifyMerchantId
 
--- delete top (1) shopify.ShopifyMerchantConfigurations
---  where ShopifyMerchantId = @ShopifyMerchantId
---  and ShopifyConfigurationId = 18
+ --delete top (12) shopify.ShopifyMerchantConfigurations
+ -- where ShopifyMerchantId = @ShopifyMerchantId
+ -- and ShopifyConfigurationId = 18
 
- insert into shopify.ShopifyMerchantConfigurations values
- (5,19, 'True')
+ --insert into shopify.ShopifyMerchantConfigurations values
+ --(5,4, 'English'),
+ --(5,1, 'True')
 
 
 
---select *
---from shopify.ShopifyMerchantConfigurations
---where ShopifyMerchantId = @ShopifyMerchantId
+select *
+from shopify.ShopifyMerchantConfigurations
+where ShopifyMerchantId = 5
 
 select *
 from shopify.ShopifyConfigurations
