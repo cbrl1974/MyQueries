dd23557906146aca56546d9a41183696dae215f1
cbrl1974 <cbrl1974@gmail.com>
2024-02-12 07:24:36 -0500

    Update query

--- Tailbasify/Inventory queries.sql
+++ Tailbasify/Inventory queries.sql
@@ -2,7 +2,7 @@
 select 
 s.id, m.merchant, s.MerchantId, s.ShopUrl, s.SecurityStamp, s.ApiVersion
 from shopify.shopifyMerchants s
-    inner join devTailbaseCore.dbo.merchants m on m.id = s.MerchantId
+    inner join datatail20130410.dbo.merchants m on m.id = s.MerchantId
 where m.id = 2798
 
 select  distinct top 100 *  from logs  WITH (NOLOCK) 
@@ -18,41 +18,40 @@ order by h.id desc
 
 
 
-select  l.* from devTailbaseCore.dbo.MerchantStores ms 
+select  l.* from datatail20130410.dbo.MerchantStores ms 
 left join Shopify.ShopifyMerchantStoreLocations l  on l.MerchantStoreId = ms.id
-where ms.MerchantID = 2798
+where ms.MerchantID = 1448
 
-select * from devTailbaseCore.dbo.merchantstoresInventory where merchantid = 2798
-select * from devTailbaseCore.dbo.MerchantInventoryTypes order by MerchantId
+select * from datatail20130410.dbo.merchantstoresInventory where merchantid = 2798
+select * from datatail20130410.dbo.MerchantInventoryTypes order by MerchantId
 
-insert into Shopify.ShopifyMerchantStoreLocations (ShopifyMerchantId, ShopifyGeneratedLocationId, MerchantStoreId, IncludeInventoryAsTags, IncludeInventory, OnlineStockThreshold)
-VALUES
-(2798, 'gid://shopify/Location/60953231528', 6031, 1,1,4),
-(2798, 'gid://shopify/Location/63336612008', 6032, 1,1,4)
+--insert into Shopify.ShopifyMerchantStoreLocations (ShopifyMerchantId, ShopifyGeneratedLocationId, MerchantStoreId, IncludeInventoryAsTags, IncludeInventory, OnlineStockThreshold)
+--VALUES
+--(2798, 'gid://shopify/Location/60953231528', 6031, 1,1,4),
+--(2798, 'gid://shopify/Location/63336612008', 6032, 1,1,4)
 
 
-exec dbo.GetBrandsForTailbaseWebsite
 
 -- update top (2) Shopify.ShopifyMerchantStoreLocations
 -- set IncludeInventoryAsTags = 1
 -- where ShopifyMerchantId = 4
 
-select * from devTailbaseCore.dbo.MerchantStores where merchantid =   2798
+select * from datatail20130410.dbo.MerchantStores where merchantid =   2798
 
-select * from devTailbaseCore.dbo.merchantstoresInventory where merchantid = 2798
-select * from devTailbaseCore.dbo.MerchantInventoryStore where merchant_id = 2798
+select * from datatail20130410.dbo.merchantstoresInventory where merchantid = 2798
+select * from datatail20130410.dbo.MerchantInventoryStore where merchant_id = 2798
 
--- update top (1) devTailbaseCore.dbo.merchantstoresInventory
+-- update top (1) datatail20130410.dbo.merchantstoresInventory
 -- set Quantity = 2
 -- where id = 13541219
 
-select * from devTailbaseCore.dbo.merchantProds where merchant_id = 2798 and productid =   44160
+select * from datatail20130410.dbo.merchantProds where merchant_id = 2798 and productid =   44160
 
 
 select sp.MerchantId, sp.id,  sp.TailbaseID, sp.handle, sp.tags, sp.SyncStatusId,sp.ModificationDate, si.Quantity, mt.InventoryType from shopify.shopifyProducts sp 
-    inner join devTailbaseCore.dbo.merchantstoresInventory si on si.MerchantId = sp.MerchantId 
+    inner join datatail20130410.dbo.merchantstoresInventory si on si.MerchantId = sp.MerchantId 
     and si.ItemId = sp.TailbaseId and si.ItemType = 1
-    inner join devTailbaseCore.dbo.MerchantInventoryTypes mt on mt.Id = si.MerchantInventoryTypeId and mt.MerchantId = sp.MerchantId
+    inner join datatail20130410.dbo.MerchantInventoryTypes mt on mt.Id = si.MerchantInventoryTypeId and mt.MerchantId = sp.MerchantId
 where sp.MerchantId = 2798
 and sp.ItemType = 1 
 and sp.tailbaseid in (44160,76142,43888)
