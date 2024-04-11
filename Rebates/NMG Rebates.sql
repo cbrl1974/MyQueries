216f96fe9796a01b010a493a7c48d21a2a7a8063
cbrl1974 <cbrl1974@gmail.com>
2024-04-11 09:43:06 -0400

    Update

--- Rebates/NMG Rebates.sql
+++ Rebates/NMG Rebates.sql
@@ -1,16 +1,31 @@
- DECLARE @MerchantIdforFeatures AS INT = 2798;
+ DECLARE @MerchantIdforFeatures AS INT = 3519;
  select * from merchantWebsiteFeatures where merchant_id = @MerchantIdforFeatures
  and featurecode like '%rebates%'
 
 --Get the nmgid(DealerId)
- select id, merchant, nmgid, NmgLocationID from merchants where id in (3384,3201)
+ select id, merchant, nmgid, NmgLocationID from merchants where id in (3499,3519)
 
 --Get the RebateFeedIds for the dealer
- select  * from RebateFeedClients where dealerid in ('8968')
+ select  * from RebateFeedClients where dealerid in ('2796') and rebatefeedid in ( 1008356, 1008361, 1008357, 1008353, 1008381, 1008151, 1008188, 1008186, 1008243, 1008244, 1008263, 1008261, 1008262, 1008375, 1008382, 1008490, 1008540, 1008555, 1008624)
 
 --Get the rebates for the dealer
- select  * from RebateFeeds 
- where RebateFeedID in (1002517,1002518,1002584,1002651,1002654,1002693,1000159,1000396,1000515,1000587,1005392,1005393,1001559,1001576,1001587,1001606,1001674,1001675,1003234,1003255,1003299,1003365,1003414,1003416,1004581,1000722,1000806,1000894,1001065,1004956,1004969,1005087,1001827,1003036,1003056,1003182,1003183,1001246,1001272,1001321,1001322,1001393,1001394,1001439,1001440,1001459,1001462,1001463,1001472,1001520,1002355,1002428,1002469,1003563,1003656,1004751,1004920,1002229,1002763,1002764,1002808,1002829,1002834,1002863,1002903,1002905,1002906,1002907,1003743)
+ select  rebatefeedid from RebateFeeds 
+ where RebateFeedID in ( select rebatefeedid from RebateFeedClients where dealerid in ('2796'))
  and active = 1
- and Start_time > '2022-11-24'
+ and End_time >= getdate()
+
+   select * from merchantProds where merchant_id = 3519 and productid in (
+   select productid from RebateFeedProducts where RebateFeedID in
+   (
+	   select  rebatefeedid from RebateFeeds 
+		 where RebateFeedID in ( 
+		 select rebatefeedid from RebateFeedClients where dealerid in ('2796'))
+		and active = 1
+		and End_time >= getdate()
+	)
+	)
+ select * from merchantrebates where merchant_id = 3499
+
+
+ select * from rebate
 
