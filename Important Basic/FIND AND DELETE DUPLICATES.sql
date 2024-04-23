b998333b26f3f77c333939f606e2afcb5731b6ea
cbrl1974 <cbrl1974@gmail.com>
2024-04-23 14:49:36 -0400

    Update

--- Important Basic/FIND AND DELETE DUPLICATES.sql
+++ Important Basic/FIND AND DELETE DUPLICATES.sql
@@ -12,3 +12,10 @@ where merchantzoneid = 1051
 )
 
 DELETE FROM CTE WHERE RN<>1
+
+--Delete collections duplicated per merchant 
+WITH CTE AS (
+    SELECT *, ROW_NUMBER() OVER (PARTITION BY merchant_ID, collectionID ORDER BY collectionID) AS RN
+    FROM merchantCollections 
+)
+delete FROM CTE WHERE RN > 1
