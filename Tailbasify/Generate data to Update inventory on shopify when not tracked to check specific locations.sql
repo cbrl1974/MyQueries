1847e324f41f0f24c09e3b8fd026113dfcb334ef
cbrl1974 <cbrl1974@gmail.com>
2024-04-29 15:35:02 -0400

    New query

---
+++ Tailbasify/Generate data to Update inventory on shopify when not tracked to check specific locations.sql
@@ -0,0 +1,24 @@
+
+select 
+sp.handle 'Handle',
+sp.TitleEn 'Title',
+'Title' as 'Option1 Name',
+'Default Title' 'Option1 Value',
+'' 'Option1 Value',
+'' 'Option2 Name',
+'' 'Option2 Value',
+'' 'Option3 Name',
+'' 'Option3 Value',
+v.sku 'SKU',
+'' 'HS Code',
+'' 'COO',
+'Entrepôt location' 'Location',
+0 'Incoming',
+0 'Unavailable',
+0 'Committed',
+0 'Available',
+0 'On hand'
+from shopify.ShopifyProductVariants v	inner join shopify.shopifyProducts sp on sp.id = v.ShopifyProductId
+where sp.merchantid = 3527
+--and v.ShopifyProductID in (1851793)
+and v.Tracked = 0
