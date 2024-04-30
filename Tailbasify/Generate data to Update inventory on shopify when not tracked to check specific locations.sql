201621b757c6d0c869085cc1a7bca5e0c0098585
cbrl1974 <cbrl1974@gmail.com>
2024-04-30 10:01:01 -0400

    Update query

--- Tailbasify/Generate data to Update inventory on shopify when not tracked to check specific locations.sql
+++ Tailbasify/Generate data to Update inventory on shopify when not tracked to check specific locations.sql
@@ -4,7 +4,6 @@ sp.handle 'Handle',
 sp.TitleEn 'Title',
 'Title' as 'Option1 Name',
 'Default Title' 'Option1 Value',
-'' 'Option1 Value',
 '' 'Option2 Name',
 '' 'Option2 Value',
 '' 'Option3 Name',
@@ -12,7 +11,7 @@ sp.TitleEn 'Title',
 v.sku 'SKU',
 '' 'HS Code',
 '' 'COO',
-'Entrepôt location' 'Location',
+'Germain Larivière Brossard' 'Location',
 0 'Incoming',
 0 'Unavailable',
 0 'Committed',
@@ -22,3 +21,55 @@ from shopify.ShopifyProductVariants v	inner join shopify.shopifyProducts sp on s
 where sp.merchantid = 3527
 --and v.ShopifyProductID in (1851793)
 and v.Tracked = 0
+and sp.ItemType = 4
+UNION
+select 
+sp.handle 'Handle',
+sp.TitleEn 'Title',
+'Title' as 'Option1 Name',
+'Default Title' 'Option1 Value',
+'' 'Option2 Name',
+'' 'Option2 Value',
+'' 'Option3 Name',
+'' 'Option3 Value',
+v.sku 'SKU',
+'' 'HS Code',
+'' 'COO',
+'Germain Larivière Laval' 'Location',
+0 'Incoming',
+0 'Unavailable',
+0 'Committed',
+0 'Available',
+0 'On hand'
+from shopify.ShopifyProductVariants v	inner join shopify.shopifyProducts sp on sp.id = v.ShopifyProductId
+where sp.merchantid = 3527
+and v.Tracked = 0
+and sp.ItemType = 4
+UNION
+select 
+sp.handle 'Handle',
+sp.TitleEn 'Title',
+'Title' as 'Option1 Name',
+'Default Title' 'Option1 Value',
+'' 'Option2 Name',
+'' 'Option2 Value',
+'' 'Option3 Name',
+'' 'Option3 Value',
+v.sku 'SKU',
+'' 'HS Code',
+'' 'COO',
+'Germain Larivière St-Hyacinthe' 'Location',
+0 'Incoming',
+0 'Unavailable',
+0 'Committed',
+0 'Available',
+0 'On hand'
+from shopify.ShopifyProductVariants v	inner join shopify.shopifyProducts sp on sp.id = v.ShopifyProductId
+where sp.merchantid = 3527
+--and v.ShopifyProductID in (1851793)
+and v.Tracked = 0
+and sp.ItemType = 4
+
+
+
+
