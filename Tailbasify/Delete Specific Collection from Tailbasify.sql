cf8e42d7693d794142be1487e62e5ce39e946ae8
cbrl1974 <cbrl1974@gmail.com>
2024-05-08 08:12:33 -0400

    Update

--- Tailbasify/Delete Specific Collection from Tailbasify.sql
+++ Tailbasify/Delete Specific Collection from Tailbasify.sql
@@ -1,13 +1,12 @@
 --Delete Specific Products Shopify
 
 -- *** Make sure that the product or collection are deleted also from the console first if exists.
-DECLARE @shopifycollectionID AS INT = 17702
+DECLARE @shopifycollectionID AS INT = 17720
 -- Delete Collections
 BEGIN TRANSACTION
-  delete from  [Shopify].[ShopifyCollectionRuleInputs] where ShopifyMerchantCollectionId in
-  (select id from shopify.ShopifyMerchantCollections where id = 17702)
-  delete top (1) from shopify.ShopifyMerchantCollections where id  = @shopifycollectionID
-  delete top (1) from shopify.ShopifyCollectionSyncReportsDetail where shopifycollectionID = @shopifycollectionID
+  delete from  [Shopify].[ShopifyCollectionRuleInputs] where ShopifyMerchantCollectionId = @shopifycollectionID
+  delete  from shopify.ShopifyMerchantCollections where id  = @shopifycollectionID
+  delete  from shopify.ShopifyCollectionSyncReportsDetail where shopifycollectionID = @shopifycollectionID
 COMMIT TRANSACTION
 
 
