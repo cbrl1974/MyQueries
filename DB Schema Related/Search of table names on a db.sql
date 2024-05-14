208d981472250656587caf2a37c8f2b5c4f9333c
cbrl1974 <cbrl1974@gmail.com>
2024-05-14 12:16:12 -0400

    Update

--- DB Schema Related/Search of table names on a db.sql
+++ DB Schema Related/Search of table names on a db.sql
@@ -4,7 +4,7 @@ WHERE TABLE_NAME like '%blog%'
 
 SELECT *
 FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_NAME = N'shopifyMerchantConfigurations'
+WHERE TABLE_NAME = N'merchantcms'
 
 SELECT
   sys.columns.name AS ColumnName,
@@ -14,5 +14,17 @@ FROM
 JOIN sys.tables ON
   sys.columns.object_id = tables.object_id
 WHERE
-  sys.columns.name like '%blog%'
+  sys.columns.name like '%auto%'
 
+--Query to get all columns except specific ones
+DECLARE @cols NVARCHAR(MAX)
+SELECT @cols = STRING_AGG(name, ', ')
+FROM sys.columns
+WHERE object_id = OBJECT_ID('merchantCMS') 
+AND name <> 'storetemplate'
+AND name <> 'storetemplate_2'
+
+DECLARE @query NVARCHAR(MAX)
+SET @query = 'SELECT TOP 1 ' + @cols + ' FROM merchantCMS WHERE merchant_id = 1448'
+
+EXEC sp_executesql @query
