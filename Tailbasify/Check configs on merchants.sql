34fb12f0c398b69fd4c537dc5ccda3d09e520610
cbrl1974 <lcabral@tailbase.com>
2024-08-26 08:10:19 -0400

    update

--- Tailbasify/Check configs on merchants.sql
+++ Tailbasify/Check configs on merchants.sql
@@ -18,6 +18,15 @@ where ShopifyMerchantId = @ShopifyMerchantId
  --(5,1, 'True')
 
 
+ select * from [DB.PROD].tailbasify.shopify.ShopifyMerchantConfigurations 
+ where shopifyConfigurationid = 22
+
+--  update top (1) [DB.PROD].tailbasify.shopify.ShopifyMerchantConfigurations
+-- set configurationValue  = '[{"id":"gid://shopify/Publication/118572417159","name":"Online Store"},{"id":"gid://shopify/Publication/118572482695","name":"Point of Sale"},{"id":"gid://shopify/Publication/118572515463","name":"Shop"},{"id":"gid://shopify/Publication/119793057927","name":"Shopify GraphiQL App"}]'
+-- where id = 464
+-- and shopifyConfigurationid = 22
+
+
 
 select *
 from shopify.ShopifyMerchantConfigurations
@@ -25,6 +34,16 @@ where ShopifyMerchantId = 5
 
 select *
 from shopify.ShopifyConfigurations
+where id = 22
 order by id
+
+select *
+from [DB.PROD].tailbasify.shopify.ShopifyConfigurations
+where id = 22
+order by id
+
+-- update top (1) [DB.PROD].tailbasify.shopify.ShopifyConfigurations
+-- set Configuration  = 'ChannelPublicationIds'
+-- where id = 22
 
 
