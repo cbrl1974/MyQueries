f733ff163f4dd76cbc0028c431fb683ceb21cc15
cbrl1974 <lcabral@tailbase.com>
2025-01-03 08:04:26 -0500

    Update

--- SPOS/Onboard SPOS.sql
+++ SPOS/Onboard SPOS.sql
@@ -1,14 +1,18 @@
 ﻿Declare @merchantID as int = 1479;
 select * from MerchantPosConfigs where merchantId = @merchantID
 
-INSERT INTO MerchantPosConfigs (MerchantId, ApiKey, LocalPath, [FileName], Config) VALUES
-(
- @merchantID,
- NEWID(),
- '/GermainLariviere/ProductInventory',
- 'ProductInventory.json',
- '{"ProcessProducts": true, "ProcessInventory": true, "ProcessMerchantItemTags": false, "ProcessAdditionalInformation": false}'
-)
+-- INSERT INTO MerchantPosConfigs (MerchantId, ApiKey, LocalPath, [FileName], Config) VALUES
+-- (
+--  @merchantID,
+--  NEWID(),
+--  '/GermainLariviere/ProductInventory',
+--  'ProductInventory.json',
+--  '{"ProcessProducts": true, "ProcessInventory": true, "ProcessMerchantItemTags": false, "ProcessAdditionalInformation": false}'
+-- )
 
 select * from MerchantPosConfigs where merchantId = @merchantID
+
+
+select top 250 * from MerchantPosLogs
+order by time desc
  
