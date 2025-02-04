8cfc8aef130f4ad5d1b2e50726668566ff78ab49
cbrl1974 <lcabral@tailbase.com>
2025-02-04 09:27:33 -0500

    Update

--- RTO/Fix issues with rto.sql
+++ RTO/Fix issues with rto.sql
@@ -6,10 +6,21 @@ order by cbl.id desc
 
 select top 10  * from MerchantPricingOptionsSettings where merchantid = 2417
 select * from  MerchantPricingOptions  where merchantid = 2417 and len(itemid) = 5  and isProduct  = 1
-select * from merchantcollections where merchant_id = 2417 and collectionID in( 32424,31998)
+select * from MerchantPricingOptions where merchantid = 2417 and  itemid in (11989,680793)
+
+select * from merchantcollections where merchant_id = 2417 and collectionID in(11989)
+select * from merchantprods where merchant_id = 2417 and productid not in (select productid from MerchantPricingOptions where merchantid = 2417 and isProduct = 1)
 select * from merchantwebsitefeatures where merchant_id = 2417
-select * from collections where id = 32424
-select * from companies where id_cie = 3181
+select * from collections where id = 11989
+select * from companies where id_cie = 321
+select * from products where id_product = 680793
+select * from categories where id_category = 633
+
+
+update top (24) MerchantPricingOptions
+set isProduct = 0 
+where merchantid = 2417 
+and len(itemid) = 5  and isProduct  = 1
 
 -- update top (6) MerchantPricingOptions 
 -- set isproduct = 0 
@@ -20,3 +31,6 @@ select * from companies where id_cie = 3181
 
 
 
+
+
+select * from companies whw
