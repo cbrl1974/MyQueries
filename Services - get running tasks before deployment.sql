5f7a06af1caf70e9defb482e9186a63db75f1115
cbrl1974 <lcabral@tailbase.com>
2025-02-10 07:28:27 -0500

    Update

--- Services - get running tasks before deployment.sql
+++ Services - get running tasks before deployment.sql
@@ -11,8 +11,4 @@ WHERE STATE = 3
                 AND (dateadd(minute, 10, getdate()) at TIME zone 'US eastern standard time')
 order by 1
 
---BestBuyCanadaScraper
-update top (1) Tailbaseservices.dbo.asynctasks
-set State = 1
-where id = '556bccba-7c26-4732-9da0-82d9d80cc1c1'
 
