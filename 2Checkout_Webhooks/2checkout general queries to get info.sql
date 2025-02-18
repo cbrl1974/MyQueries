53c436afe9e4d4039c593f4d70eaed63a68829ba
cbrl1974 <lcabral@tailbase.com>
2025-02-18 14:23:24 -0500

    Update

--- 2Checkout_Webhooks/2checkout general queries to get info.sql
+++ 2Checkout_Webhooks/2checkout general queries to get info.sql
@@ -1,9 +1,12 @@
-select * from sales  
+select top 10
+    *
+from sales
 --where AVANGATE_CUSTOMER_REFERENCE in (118398344)
 order by message_id desc
 --delete from sales
 
-select id, FIRSTNAME, LASTNAME, ADDRESS1, city, [STATE], ZIPCODE, country, COUNTRYCODE from CustomerInformation 
+select id, FIRSTNAME, LASTNAME, ADDRESS1, city, [STATE], ZIPCODE, country, COUNTRYCODE
+from CustomerInformation
 --where id = 881058379
 order by creationdate desc
 
@@ -11,14 +14,31 @@ order by creationdate desc
 --delete  from CustomerInformation
 
 
-select * from Subscriptions where AVANGATE_CUSTOMER_REFERENCE in (557180423)
+select *
+from Subscriptions
+where AVANGATE_CUSTOMER_REFERENCE in (557180423)
 --delete  from Subscriptions
 
-select * from SubscriptionsUsers where CUSTOMER_ID in (557180423)
+select *
+from SubscriptionsUsers
+where CUSTOMER_ID in (557180423)
 --delete from SubscriptionsUsers
 
-select * from PaymentCodes
-select * from messages
+select *
+from PaymentCodes
+select *
+from messages
+
+delete from sales where AVANGATE_CUSTOMER_REFERENCE = '121212121'
+delete from CustomerInformation where id =  121212121
+delete from Subscriptions where AVANGATE_CUSTOMER_REFERENCE = '121212121'
+delete from SubscriptionsUsers where CUSTOMER_ID = '121212121'
+delete from WebHooksLogs
+
+--select * from CustomerInformation where id =  888888888
+--select * from Subscriptions where AVANGATE_CUSTOMER_REFERENCE = '888888888'
+--select *  from SubscriptionsUsers where CUSTOMER_ID = '888888888'
+--select *  from WebHooksLogs
 
 
 
