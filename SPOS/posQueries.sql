ac4bc5660dee6ac0464562fc6ae63d908542ab8f
cbrl1974 <lcabral@tailbase.com>
2025-02-18 07:29:44 -0500

    Update

--- SPOS/posQueries.sql
+++ SPOS/posQueries.sql
@@ -1,5 +1,7 @@
 --When onboarding you need to add the new merchant here 
-select * from MerchantPosConfigs
+
+select * from datatail20130410.dbo.MerchantPosConfigs
 
 --CheckLogs
-select * from MerchantPosLogs where merchantid = 2798
+select top 250 * from datatail20130410.dbo.MerchantPosLogs 
+order by time desc
