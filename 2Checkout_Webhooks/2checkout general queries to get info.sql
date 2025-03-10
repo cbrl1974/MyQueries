d013fbaa5c83be50b86dbda844ce18d9848147a4
cbrl1974 <lcabral@tailbase.com>
2025-03-10 08:24:49 -0400

    Update

--- 2Checkout_Webhooks/2checkout general queries to get info.sql
+++ 2Checkout_Webhooks/2checkout general queries to get info.sql
@@ -1,47 +1,50 @@
 use Copernic_verifone
 
-
-select top 10
+select top 50
     *
 from sales
---where AVANGATE_CUSTOMER_REFERENCE in (118398344)
-order by message_id desc
---delete from sales
+where saledate > convert(date,getdate()-1)
+order by saledate desc
+
+--total sale per day
+SELECT 
+    CONVERT(DATE, SALEDATE) AS SALEDATE, 
+    Format(SUM(IPN_TOTAL * FX_RATE), 'N2') AS totalSale
+FROM sales
+GROUP BY CONVERT(DATE, SALEDATE)
+ORDER BY SALEDATE DESC;
+
+
 
 select id, FIRSTNAME, LASTNAME, ADDRESS1, city, [STATE], ZIPCODE, country, COUNTRYCODE
 from CustomerInformation
---where id = 881058379
 order by creationdate desc
 
 
---delete  from CustomerInformation
+
 
 
 select *
 from Subscriptions
 where AVANGATE_CUSTOMER_REFERENCE in (557180423)
---delete  from Subscriptions
+
 
 select *
 from SubscriptionsUsers
 where CUSTOMER_ID in (557180423)
---delete from SubscriptionsUsers
+
 
 select *
 from PaymentCodes
 select *
 from messages
 
-select *  from WebHooksLogs
+select *
+from WebHooksLogs
 where creationDate > convert(date,getdate()-1)
 --and severity <> 0
 order by creationDate desc
 
---delete from sales where AVANGATE_CUSTOMER_REFERENCE = '121212121'
---delete from CustomerInformation where id =  121212121
---delete from Subscriptions where AVANGATE_CUSTOMER_REFERENCE = '121212121'
---delete from SubscriptionsUsers where CUSTOMER_ID = '121212121'
---delete from WebHooksLogs
 
 
 
