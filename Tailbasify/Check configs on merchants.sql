80f32969855ea85c2b36b51a38e2baa61caee35e
cbrl1974 <lcabral@tailbase.com>
2025-04-04 15:48:26 -0400

    Update

--- Tailbasify/Check configs on merchants.sql
+++ Tailbasify/Check configs on merchants.sql
@@ -1,41 +1,50 @@
 --**************************Configs
-use devtailbasify
-DECLARE @MerchantId as int = 1448;
+use tailbasify
+DECLARE @MerchantId as int = 2802;
 DECLARE @ShopifyMerchantId as int =  (select s.id
-from devtailbasify.shopify.shopifyMerchants s
-    inner join DevTailbaseCore.dbo.merchants m on m.id = s.MerchantId
+from tailbasify.shopify.shopifyMerchants s
+    inner join datatail20130410.dbo.merchants m on m.id = s.MerchantId
 where m.id = @MerchantId);
 
 select @ShopifyMerchantId as ShopifyMerchantId, c.id, c.Configuration, sc.ConfigurationValue
-from devtailbasify.shopify.ShopifyMerchantConfigurations sc
-    inner join devtailbasify.shopify.ShopifyConfigurations c on c.Id = sc.ShopifyConfigurationId
+from tailbasify.shopify.ShopifyMerchantConfigurations sc
+    inner join tailbasify.shopify.ShopifyConfigurations c on c.Id = sc.ShopifyConfigurationId
 where ShopifyMerchantId = @ShopifyMerchantId
+
+select sm.id, m.active, sm.MerchantId, sm.ShopUrl, sm.AccessToken from shopify.ShopifyMerchants sm
+inner join datatail20130410.dbo.merchants m on m.id = sm.MerchantId
+where sm.id not in (select ShopifyMerchantId from tailbasify.shopify.ShopifyMerchantConfigurations
+where ShopifyConfigurationId = 22)
+and m.active = 1
+order by sm.merchantid desc
+
+insert into tailbasify.shopify.ShopifyMerchantConfigurations
+ values
+     (19, 22, '[{"id":"gid://shopify/Publication/88933007592","name":"Online Store"},{"id":"gid://shopify/Publication/89588891880","name":"Shopify GraphiQL App"}]')
+
+
 
 
 delete top (1) Tailbasify.shopify.ShopifyMerchantConfigurations 
 where ShopifyMerchantId = 38 
 and ShopifyConfigurationId = 19
 
-update top (1) DevTailbasify.shopify.ShopifyMerchantConfigurations
+update top (1) tailbasify.shopify.ShopifyMerchantConfigurations
 set ConfigurationValue = 'yes'
 where ShopifyMerchantId = 4
     and ShopifyConfigurationId = 23
 
 
 select *
-from DevTailbasify.shopify.ShopifyMerchantConfigurations
+from tailbasify.shopify.ShopifyMerchantConfigurations
 
-delete top (12) DevTailbasify.shopify.ShopifyMerchantConfigurations
+delete top (12) tailbasify.shopify.ShopifyMerchantConfigurations
  where ShopifyMerchantId = 4
  and ShopifyConfigurationId = 23
 
- insert into DevTailbasify.shopify.ShopifyMerchantConfigurations
- values
-     (4, 23, 'yes')
-
-
+ 
 select *
-from devtailbasify.shopify.ShopifyMerchantConfigurations
+from tailbasify.shopify.ShopifyMerchantConfigurations
 where shopifyConfigurationid = 22
 
 
@@ -45,7 +54,7 @@ from shopify.ShopifyMerchantConfigurations
 where ShopifyMerchantId = 5
 
 select *
-from devtailbasify.shopify.ShopifyConfigurations
+from tailbasify.shopify.ShopifyConfigurations
 where id = 22
 order by id
 
@@ -54,7 +63,7 @@ from tailbasify.shopify.ShopifyConfigurations
 where id = 22
 order by id
 
--- update top (1) devtailbasify.shopify.ShopifyConfigurations
+-- update top (1) tailbasify.shopify.ShopifyConfigurations
 -- set Configuration  = 'ChannelPublicationIds'
 -- where id = 23
 
