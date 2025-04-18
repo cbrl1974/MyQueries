575328d874b1bbab65f8811224776db9a2cfff55
cbrl1974 <lcabral@tailbase.com>
2025-04-18 07:40:06 -0400

    Update

--- 2Checkout_Webhooks/2checkout general queries to get info.sql
+++ 2Checkout_Webhooks/2checkout general queries to get info.sql
@@ -1,14 +1,23 @@
 use Copernic_verifone
 
-select top 50
-    *
+select 
+    IPN_RESELLER_ID, 
+	AVANGATE_CUSTOMER_REFERENCE,EXTERNAL_CUSTOMER_REFERENCE,
+	IPN_PARTNER_CODE, ORDERSTATUS,
+	*
 from Copernic_verifone.dbo.sales
+--where REFNO = '115052909'
+--where IPN_PARTNER_CODE is not null
+--where REFNO = 252673919
+--where  AVANGATE_CUSTOMER_REFERENCE = '47474788'
+--where saledate between 2014-01-01 and 2015-07-31
 where saledate > convert(date,getdate()-1)
 order by saledate desc
 
 
 select count(message_id)
 from Copernic_verifone.dbo.sales
+--120622
 
 --total sale per day
 SELECT
@@ -30,7 +39,7 @@ order by creationdate desc
 
 select *
 from Copernic_verifone.dbo.Subscriptions
-where AVANGATE_CUSTOMER_REFERENCE in (557180423)
+where LAST_ORDER_REFERENCE in (115052909)
 
 
 select *
@@ -40,17 +49,18 @@ where CUSTOMER_ID in (557180423)
 
 select *
 from Copernic_verifone.dbo.PaymentCodes
-select *
-from messages
 
-
-
-select *
+select top 200 *
 from Copernic_verifone.dbo.WebHooksLogs
 where 1 = 1
---where creationDate > convert(date,getdate()-1)
+and creationDate > convert(date,getdate()-1)
 and severity <> 0
+--and module = 'HandleIpn'
 order by creationDate desc
+
+
+
+
 
 
 
