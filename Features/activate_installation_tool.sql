f856426a63d019832da877df42cabb50a46a54fe
cbrl1974 <lcabral@tailbase.com>
2025-04-22 10:51:36 -0400

    Update

--- Features/activate_installation_tool.sql
+++ Features/activate_installation_tool.sql
@@ -1,10 +1,20 @@
 
-
 DECLARE @InstallationTool AS varchar(200) = 'installationsSettings';
 DECLARE @MerchantID AS int = 2217;
 
+--Check if orders in the merchantcms table
+select orders from merchantCms where merchant_id = @MerchantID 
+
 --Check if Installation tool already in place
 select * from merchantwebsitefeatures where merchant_id = @MerchantID and featurecode =  @InstallationTool
+
+
+--Activate orders
+update top (1) merchantCms
+set orders = 1
+where merchant_id =  @MerchantID
+
+
 
 --Add Installation Tool
 insert into merchantwebsitefeatures (merchant_id, featureCode) VALUES
