017f9413c2ddf24c710b89ccce82fbf88e12795a
cbrl1974 <lcabral@tailbase.com>
2025-07-11 08:05:45 -0400

    update

--- Tailbasify/FIX SHOPIFY VARIANTS DUPLICATES.sql
+++ Tailbasify/FIX SHOPIFY VARIANTS DUPLICATES.sql
@@ -1,3 +1,5 @@
+use Tailbasify
+
 --CHECK EXPORTS
 select  me.MerchantId,merchant, me.Status, me.ModificationDate, me.ShopifyMerchantId
 from tailbasify.dbo.MerchantExports me WITH (NOLOCK)
@@ -13,7 +15,7 @@ from shopify.ShopifyProductVariants v
 	inner join shopify.shopifyProducts sp on sp.id = v.ShopifyProductId
 	where 1 = 1
     and sp.merchantid not in (3209,2798,1956)
-	--and sp.merchantid  = 2384
+	and sp.merchantid  = 3523
 and v.ShopifyGeneratedVariantId is null
 --and sp.SyncStatusId = 4
 order by sp.MerchantId
@@ -31,7 +33,7 @@ and v.ShopifyGeneratedVariantId is null
 --CHECK SYNC FLOW
 select distinct  merchantid, text,logtime, category module 
 from logs  WITH (NOLOCK) 
-WHERE  merchantid = 2248
+WHERE  merchantid = 3527
 and LogTime > convert(date,getdate()-0)
 order by LogTime desc 
 
@@ -45,3 +47,26 @@ order by h.id desc
 
 --CHECK FOR INFO ON VARIANT
 select * from shopify.ShopifyProductVariants where id = 3344786
+
+
+
+--Update = 1
+--Convert only = 2 
+--Synchronize only = 3
+
+Declare @action as int = 3;
+
+-- Merchants
+select
+    s.MerchantId, s.id, m.merchant,
+	case
+	when @action = 1  then 'https://api.tailbasify.com/'+ s.ApiVersion + '/api/Shopify/Update/' + s.SecurityStamp 
+	WHEN  @action = 2 then 'https://api.tailbasify.com/'+ s.ApiVersion + '/api/Converter/Convert/' + s.SecurityStamp 
+	WHEN  @action = 3 then 'https://api.tailbasify.com/'+ s.ApiVersion + '/api/Synchronizer/Synchronize/' + s.SecurityStamp 
+	  END AS EndPoint,
+	 s.ShopUrl, s.SecurityStamp, s.AccessToken, ApiKey
+from shopify.shopifyMerchants s
+    inner join datatail20130410.dbo.merchants m on m.id = s.MerchantId
+	and s.merchantid = 3523
+	and m.active = 1
+order by s.id
