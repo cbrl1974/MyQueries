8750d1f591720e81fef56c8816738a7269ce5b05
cbrl1974 <lcabral@tailbase.com>
2025-07-23 14:20:00 -0400

    Update

--- Reports/Get ALL merchants brands counts and brands count based on products.sql
+++ Reports/Get ALL merchants brands counts and brands count based on products.sql
@@ -3,6 +3,17 @@ WITH TailbasiFyMerchants AS (
     FROM merchants mi
     WHERE mi.active = 1
 ),
+merchantSolutions AS (
+ SELECT DISTINCT mwi.merchant_id, mwi.infoValue as solution
+    FROM MerchantWebsiteInformation mwi
+    WHERE mwi.infoCode = 'template'
+	and mwi.infoValue like '%cantrex%'
+),
+TotalproductsPerMerchant AS (
+ SELECT merchant_id, count(productid) as totalProducts
+    FROM merchantprods 
+	group by merchant_id
+),
 merchantBrandsCount AS (
     SELECT 
         mb.merchant_id,
@@ -33,9 +44,13 @@ productsBrandsCount AS (
 SELECT 
     tm.id, 
     tm.merchant, 
-    tm.AccountingID, 
+    --tm.AccountingID,
+	ms.solution,
+	tp.totalProducts,
     mbc.cieid AS 'Brands Count'   
 FROM TailbasiFyMerchants tm
+inner join merchantSolutions ms on ms.merchant_id = tm.ID
 INNER JOIN merchantBrandsCount mbc ON mbc.merchant_id = tm.id
 INNER JOIN productsBrandsCount pbc ON pbc.merchant_id = tm.id
+inner join TotalproductsPerMerchant tp on tp.merchant_id = tm.ID
 ORDER BY tm.id;
