2d7237c2eae1490fdebe4551aaad93a5221f8485
cbrl1974 <lcabral@tailbase.com>
2026-02-02 08:33:56 -0500

    updates

--- Elastic search/check elastic search.sql
+++ Elastic search/check elastic search.sql
@@ -4,4 +4,26 @@ EXECUTE dbo.ElasticSearch_Update_Procedure '3065','',1
 select * from ElasticSearch_trackedchanges
 
 
+  select * from ElasticSearch_TrackedChanges 
+  order by creationDate
+  
+  select merchantid, count(merchantid) from ElasticSearch_TrackedChanges 
+  group by MerchantId
+  order by count(merchantid) desc
+  --where merchantid = 2547
+
+   SELECT TOP(600) * 
+  FROM Stats.TableStats 
+  WHERE SchemaName = 'dbo' AND TableName = 'ElasticSearch_TrackedChanges'
+  ORDER BY Id DESC
+
+  select top 50  * from ElasticSearchSettings
+
+  --update top (1) ElasticSearchSettings
+  --set value = 4000
+  --where textcode = 'UpdaterBatchSize'
+  --and id = 21
+
+
+
 
