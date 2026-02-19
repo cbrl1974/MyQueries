c9317e8410d197a683ae0367e9a474f1f1c7ab6c
cbrl1974 <lcabral@tailbase.com>
2026-02-19 07:54:52 -0500

    Update

--- Shopping Carts/Get last transaction successfull date.sql
+++ Shopping Carts/Get last transaction successfull date.sql
@@ -2,25 +2,32 @@
 select l.merchant_id, m.merchant, m.active, max(l.creationTimestamp) as lastCreationOrder
 from MerchantTransactionLog l
 	join merchants m on m.id = l.merchant_id
+	join MerchantWebsiteInformation i on i.merchant_id = m.id and i.infoCode = 'template' and i.infoValue not like '%tailbasify%'
 where l.merchant_id in (
 	select merchant_id
 	from merchantwebsitefeatures
 	where featurecode = 'shoppingCartType' 
 ) and l.customerOrderId is not null
 	and l.txnservice not like '%PayPal Checkout Order Transaction SANDBOX TEST%'
+	and m.active = 1
 group by l.merchant_id,m.merchant,m.active
 order by lastCreationOrder desc
+
+
+
 
 --Check last movement transaction date for the merchants 
 select l.merchant_id, m.merchant, m.active, max(l.creationTimestamp) as lastCreationOrder
 from MerchantTransactionLog l
 	join merchants m on m.id = l.merchant_id
+	join MerchantWebsiteInformation i on i.merchant_id = m.id and i.infoCode = 'template' and i.infoValue not like '%tailbasify%'
 where l.merchant_id in (
 	select merchant_id
 	from merchantwebsitefeatures
 	where featurecode = 'shoppingCartType' 
 )
 	and l.txnservice not like '%PayPal Checkout Order Transaction SANDBOX TEST%'
+	and m.active = 1
 group by l.merchant_id,m.merchant,m.active
 order by lastCreationOrder desc
 
