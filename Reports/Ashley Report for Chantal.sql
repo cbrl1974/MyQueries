83082ef2ab7a82ca8618b27e78230a49b1b8bac7
cbrl1974 <lcabral@tailbase.com>
2026-03-25 15:51:02 -0400

    Update

--- Reports/Ashley Report for Chantal.sql
+++ Reports/Ashley Report for Chantal.sql
@@ -1,30 +1,49 @@
-
-;WITH cleaned_merchants AS (
-    SELECT 
-        m.id,
-        m.merchant,
-        m.merchant_url,
-        m.active AS isActive,
-        mf.remoteFTPUser,
-        mfe.FeedOptionsJson,
-        mfe.ModifiedOn,
-        mfe.merchantid AS mfe_merchantid,
-        mf.lastModified AS rawLastModified,
-        mf.active AS isAshleyFeedActive,
-        COALESCE(
+;WITH
+    cleaned_merchants
+    AS
+    (
+        SELECT
+            'Tailbase' as Company,
+            m.id,
+            m.merchant,
+            m.merchant_url,
+            m.active AS isActive,
+            mf.remoteFTPUser,
+            mfe.FeedOptionsJson,
+            mfe.ModifiedOn,
+            mfe.merchantid AS mfe_merchantid,
+            mf.lastModified AS rawLastModified,
+            mf.active AS isAshleyFeedActive,
+            COALESCE(
             TRY_CAST(REPLACE(REPLACE(mf.lastModified, '{ts ''', ''), '''}', '') AS datetime),
             mfe.ModifiedOn
         ) AS lastmodified,
-        COALESCE(
+            COALESCE(
             SUBSTRING(mf.remoteFTPUser, 4, 6),
             JSON_VALUE(mfe.FeedOptionsJson, '$.CustomerId')
         ) AS AccountNumber
-    FROM merchants m
-    LEFT JOIN merchantfeeds mf ON m.id = mf.merchant_id
-    LEFT JOIN feeds.merchantfeeds mfe ON mfe.merchantid = m.id AND mfe.FeedId = 9
-)
+        FROM merchants m
+            LEFT JOIN merchantfeeds mf ON m.id = mf.merchant_id
+            LEFT JOIN feeds.merchantfeeds mfe ON mfe.merchantid = m.id AND mfe.FeedId = 9
+    ),
 
-SELECT 
+    ashley_pages
+    AS
+    (
+        SELECT DISTINCT
+            mwp.merchant_id,
+            'YES' AS HasAshleyLandingPage
+        FROM merchantwebsitepages mwp
+        WHERE mwp.name LIKE '%Ashley%'
+            OR mwp.name LIKE '%Ashley Sleep%'
+            OR mwp.name LIKE '%Benchcraft%'
+            OR mwp.name LIKE '%Millennium%'
+            OR mwp.name LIKE '%Sierra Sleep%'
+            OR mwp.name LIKE '%Signature Design%'
+    )
+
+SELECT
+    cm.Company,
     cm.id,
     MAX(cm.merchant) AS merchant,
     MAX(cm.AccountNumber) AS AccountNumber,
@@ -56,27 +75,35 @@ SELECT
     MAX(md.statecode) AS statecode,
     MAX(md.CountryName) AS CountryName,
     MAX(cm.lastmodified) AS lastmodified,
-MAX(
-    'https://www.google.com/maps/place/' +
-    REPLACE(
-        CONCAT(
-            ISNULL(mstore.address, ''), ' ',
-            ISNULL(mstore.city, ''), ', ',
-            ISNULL(mstore.province, ''), ' ',
-            ISNULL(mstore.postalcode, ''), ', ',
-            ISNULL(mstore.Country, '')
-        ),
-        ' ',
-        '+'
-    ) +
-    '/@' + mstore.GeoPosition + ',17z'
-) AS LocationUrl,
+
+    MAX(
+        'https://www.google.com/maps/place/' +
+        REPLACE(
+            CONCAT(
+                ISNULL(mstore.address, ''), ' ',
+                ISNULL(mstore.city, ''), ', ',
+                ISNULL(mstore.province, ''), ' ',
+                ISNULL(mstore.postalcode, ''), ', ',
+                ISNULL(mstore.Country, '')
+            ),
+            ' ',
+            '+'
+        ) +
+        '/@' + mstore.GeoPosition + ',17z'
+    ) AS LocationUrl,
+
     COUNT(DISTINCT mp.productid) AS AshleyProdsOnWebsite,
+
     MAX(CASE 
         WHEN cm.isAshleyFeedActive = 1 THEN 'yes' 
         WHEN cm.mfe_merchantid IS NOT NULL THEN 'yes'  
         ELSE 'no' 
     END) AS IsAshleyFeedActiveInTheConsole,
+
+    MAX(CASE 
+        WHEN ap.HasAshleyLandingPage IS NOT NULL THEN 'YES'
+        ELSE 'NO'
+    END) AS HasAshleyLandingPage,
 
     MAX(mstore.city)        AS City,
     MAX(mstore.province)    AS Province,
@@ -90,8 +117,10 @@ FROM cleaned_merchants cm
     INNER JOIN merchantCms mc ON mc.Merchant_id = cm.id
     INNER JOIN MerchantWebsiteInformation mwi ON mwi.merchant_id = cm.id
     INNER JOIN merchantBrands mb ON mb.merchant_id = cm.id AND mb.cieId = p.manufID
+    LEFT JOIN ashley_pages ap ON ap.merchant_id = cm.id
 OUTER APPLY (
-    SELECT TOP 1 GeoPosition, city, province, postalcode, Country, address
+    SELECT TOP 1
+        GeoPosition, city, province, postalcode, Country, address
     FROM MerchantStores ms2
     WHERE ms2.MerchantID = cm.id
     ORDER BY ms2.ID
@@ -105,6 +134,5 @@ WHERE
     AND (cm.rawLastModified IS NOT NULL OR cm.mfe_merchantid IS NOT NULL)
     AND p.manufID IN (1486, 6183, 3181, 3182, 3184, 4226, 4227)
 
-GROUP BY cm.id
-ORDER BY cm.id;
-
+GROUP BY cm.id, cm.Company
+ORDER BY cm.id, cm.Company;
