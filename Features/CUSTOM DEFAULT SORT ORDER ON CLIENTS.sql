c3c241998ec2b9677b9a45b4728dfb511fef114a
cbrl1974 <lcabral@tailbase.com>
2026-05-14 08:17:59 -0400

    Update

--- Features/CUSTOM DEFAULT SORT ORDER ON CLIENTS.sql
+++ Features/CUSTOM DEFAULT SORT ORDER ON CLIENTS.sql
@@ -1,8 +1,8 @@
-use datatail20130410
+use stagingtailbasecore
 
 --Custom order to display product in stock first by using Sinray as examples
 Declare @merchantId as int = 3142; --
-Declare @merchantIdToChange as int = 3468; --
+Declare @merchantIdToChange as int = 3468; --?
 insert into  merchantWebsitefeatures (merchant_id, featurecode, featureValue) 
 select @merchantIdToChange, featurecode, featureValue
 from merchantWebsitefeatures
@@ -11,7 +11,7 @@ and featurecode like '%sort%'
 
 select m.id, m.merchant, mf.* from merchantwebsitefeatures mf
 join merchants m on m.id = mf.merchant_id
-where mf.featurecode like '%defaultSort%'
+where mf.featurecode like '%Sort%'
 and  mf.merchant_id in (select merchant_id  from merchantwebsitefeatures
 where featurevalue    in ('tbplatinum') and featureCode = 'template')
 order by mf.featurecode
@@ -33,9 +33,49 @@ set featureValue = '(CASE WHEN mp.displayorder IS NULL THEN 1 ELSE 0 END), ISNUL
 where merchant_id = @merchantIdForUpdateFeatures
 and featurecode  =  'defaultSort'
 
+SELECT * from merchantwebsitefeatures where merchant_id = 3165 and featurecode like '%search%'
 
 -- Delete feature
-Declare @merchantIdForDeleteFeatures as int = 3361;
-DELETE TOP (1) merchantwebsitefeatures WHERE MERCHANT_ID = @merchantIdForDeleteFeatures
-AND Featurecode = 'defaultSort'
+
+DELETE TOP (1) merchantwebsitefeatures WHERE MERCHANT_ID = 3165
+AND Featurecode = 'hasCollectionSearch'
+
+DELETE TOP (1) merchantwebsitefeatures WHERE MERCHANT_ID = 3165
+AND Featurecode = 'searchDefaultSort'
+
+
+DELETE TOP (1) merchantwebsitefeatures WHERE MERCHANT_ID = 3165
+AND Featurecode = 'hasCollectionsFirstOnSearch'
+
+
+
+
+
+insert into merchantwebsitefeatures values 
+(3165,'hasCollectionsFirstOnSearch',1)
+
+insert into merchantwebsitefeatures values 
+(3165,'searchDefaultSort', 'price asc')
+
+insert into merchantwebsitefeatures values 
+(3165,'hasCollectionSearch', '')
+
+update top (1) merchantwebsitefeatures
+set featureValue = 'price asc'
+where merchant_id = 3165 
+and featurecode = 'searchDefaultSort'
+
+update top (1) merchantwebsitefeatures
+set featureValue = 1
+where merchant_id = 3165 
+and featurecode = 'hasCollectionsFirstOnSearch'
+
+
+select * from merchantwebsitepages where merchant_id = 3438 and name = '2026-01-WHR MAY'
+
+select * from merchants where id = 3669
+
+
+
+
 
